3b67a30f9f4b84b2403479697d4de73ed0fce640
JacksonCampolattaro <jackcamp@vt.edu>
2018-11-06 13:39:49 -0500

    Body passes all method tests.

--- tests/bodyTest.cpp
+++ tests/bodyTest.cpp
@@ -6,62 +6,60 @@
 #include "../src/n_body/body.h"
 
 
-TEST(bodyTest, bodyTest_applyVelocity_Test) {
+class bodyTest : public ::testing::Test {
+protected:
 
-    auto theBody = new body(vec3(0, 0, 0), vec3(1, 0, 0), 18000, 1000, vec3(1, 1, 1));
+    virtual void SetUp() {
+        theBody = new body(vec3(0, 0, 0), vec3(1, 0, 0), 2, 1, vec3(1, 1, 1));
+    }
+
+    virtual void TearDown() {
+        delete theBody;
+    }
+
+    body *theBody;
+};
+
+TEST_F(bodyTest, bodyTest_applyVelocity_Test) {
 
     theBody->applyVelocity(1);
     EXPECT_EQ(vec3(1, 0, 0), theBody->getVelocity());
     EXPECT_EQ(vec3(1, 0, 0), theBody->getPosition());
 }
 
-TEST(bodyTest, bodyTest_applyForce_Test) {
-
-    auto theBody = new body(vec3(0, 0, 0), vec3(0, 0, 0), 2, 1, vec3(1, 1, 1));
+TEST_F(bodyTest, bodyTest_applyForce_Test) {
 
     theBody->applyForce(vec3(1, 0, 0), 2);
-    EXPECT_EQ(vec3(1, 0, 0), theBody->getVelocity());
+    EXPECT_EQ(vec3(2, 0, 0), theBody->getVelocity());
 }
 
-TEST(bodyTest, bodyTest_addVelocity_Test) {
-
-    auto theBody = new body(vec3(0, 0, 0), vec3(0, 0, 0), 2, 1, vec3(1, 1, 1));
+TEST_F(bodyTest, bodyTest_addVelocity_Test) {
 
     theBody->addVelocity(vec3(1, 0, 0));
-    EXPECT_EQ(vec3(1, 0, 0), theBody->getVelocity());
+    EXPECT_EQ(vec3(2, 0, 0), theBody->getVelocity());
 }
 
-TEST(bodyTest, bodyTest_getPosition_Test) {
-
-    auto theBody = new body(vec3(0, 0, 0), vec3(0, 0, 0), 2, 1, vec3(1, 1, 1));
+TEST_F(bodyTest, bodyTest_getPosition_Test) {
 
     EXPECT_EQ(vec3(0, 0, 0), theBody->getPosition());
 }
 
-TEST(bodyTest, bodyTest_getVelocity_Test) {
+TEST_F(bodyTest, bodyTest_getVelocity_Test) {
 
-    auto theBody = new body(vec3(0, 0, 0), vec3(2, 0, 0), 2, 1, vec3(1, 1, 1));
-
-    EXPECT_EQ(vec3(2, 0, 0), theBody->getVelocity());
+    EXPECT_EQ(vec3(1, 0, 0), theBody->getVelocity());
 }
 
-TEST(bodyTest, bodyTest_getMass_Test) {
+TEST_F(bodyTest, bodyTest_getMass_Test) {
 
-    auto theBody = new body(vec3(0, 0, 0), vec3(0, 0, 0), 18000, 1000, vec3(1, 1, 1));
-
-    EXPECT_NEAR(18000, theBody->getMass(), 0.1);
+    EXPECT_NEAR(2, theBody->getMass(), 0.1);
 }
 
-TEST(bodyTest, bodyTest_getRadius_Test) {
+TEST_F(bodyTest, bodyTest_getRadius_Test) {
 
-    auto theBody = new body(vec3(0, 0, 0), vec3(0, 0, 0), 10000, 1000, vec3(1, 1, 1));
-
-    EXPECT_NEAR(1.33650, theBody->getRadius(), 0.1);
+    EXPECT_NEAR(0.781592607, theBody->getRadius(), 0.1);
 }
 
-TEST(bodyTest, bodyTest_getColor_Test) {
-
-    auto theBody = new body(vec3(0, 0, 0), vec3(3, 2, 1), 10000, 1000, vec3(1, 1, 1));
+TEST_F(bodyTest, bodyTest_getColor_Test) {
 
     EXPECT_EQ(vec3(1, 1, 1), theBody->getColor());
 }
