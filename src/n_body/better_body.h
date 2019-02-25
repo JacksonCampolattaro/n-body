cc23f57a688c865ba049b105a4ea5f3809a77e6c
Jackson Campolattaro <jackcamp@vt.edu>
2019-02-25 01:02:29 -0500

    Starting on a streamlined rewrite of the body class.

--- src/n_body/better_body.h
+++ src/n_body/better_body.h
@@ -6,7 +6,27 @@
 #define N_BODY_BETTER_BODY_H
 
 
-class better_body {
+#include <iostream>
+#include <string>
+
+#include <glm/glm.hpp>
+#include <GLFW/glfw3.h>
+
+#include "drawable.h"
+
+/**
+ * Intended to streamline the functionality of the old body
+ * The goal is to retain all the same features, while reducing the number of methods.
+ */
+class better_body : public drawable {
+public:
+
+private:
+
+
+    glm::vec3 position;
+    glm::vec3 velocity;
+
 
 };
 
