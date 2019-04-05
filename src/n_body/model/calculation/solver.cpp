ea27c7e1fc5d5303b366bc79d4bdc1499757a967
Jackson Campolattaro <jackcamp@vt.edu>
2019-04-05 16:23:27 -0400

    Splitting up calculations using an abstract solver class.

---
+++ src/n_body/model/calculation/solver.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jackcamp on 4/5/19.
+//
+
+#include "solver.h"
---
+++ src/n_body/model/calculation/solver.h
@@ -0,0 +1,14 @@
+//
+// Created by jackcamp on 4/5/19.
+//
+
+#ifndef N_BODY_SOLVER_H
+#define N_BODY_SOLVER_H
+
+
+class solver {
+
+};
+
+
+#endif //N_BODY_SOLVER_H
