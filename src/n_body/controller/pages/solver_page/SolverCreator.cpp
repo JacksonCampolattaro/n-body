775a515a46a5f7d7d978bf8ed1bcfd29431f468e
jackcamp <jackcamp@vt.edu>
2019-06-17 10:29:53 -0400

    Solver creator is fully functional.

--- src/n_body/controller/pages/solver_page/SolverCreator.cpp
+++ src/n_body/controller/pages/solver_page/SolverCreator.cpp
@@ -2,7 +2,10 @@
 // Created by jackcamp on 6/12/19.
 //
 
+#include <iostream>
 #include "SolverCreator.h"
+#include "../../../model/calculation/BarnesHut/BarnesHutSolver.h"
+#include "../../../model/calculation/Naive/NaiveSolver.h"
 
 SolverCreator::SolverCreator() {
 
@@ -30,14 +33,55 @@ SolverCreator::SolverCreator() {
 
 
     // Configuring the combo box
-    comboBox_solver.set_active_text("Barnes-Hut Algorithm");
+
+    solver_comboBox.append("Barnes-Hut Algorithm");
+    solver_comboBox.append("Naive Algorithm");
+    solver_comboBox.set_active(0);
+    solver_comboBox.signal_changed().connect(sigc::mem_fun(*this, &SolverCreator::on_combo_changed));
+
+
+    // Configuring the theta label
+
+    theta_label.set_alignment(Gtk::ALIGN_START);
+
+
+    // Configuring the theta spinButton
+
+    theta_spinButton.set_range(0, 100);
+    theta_spinButton.set_digits(3);
+    theta_spinButton.set_increments(0.01, 0.1);
+    theta_spinButton.set_value(0.90);
+    theta_spinButton.set_snap_to_ticks(false);
+
+
+    // Configuring the theta box
+    theta_box.pack_start(theta_label);
+    theta_box.pack_start(theta_spinButton);
+
+
+    // Configuring the threading label
+
+    threading_label.set_alignment(Gtk::ALIGN_START);
+
+
+    // Configuring the threading switch
+
+    threading_switch.set_active(true);
+
+
+    // Configuring the threading box
+
+    threading_box.pack_start(threading_label);
+    threading_box.pack_start(threading_switch, Gtk::PACK_SHRINK);
 
 
     // Arranging the main box
 
     pack_start(header_label, Gtk::PACK_SHRINK);
     pack_start(description_label, Gtk::PACK_SHRINK);
-    pack_start(comboBox_solver, Gtk::PACK_SHRINK);
+    pack_start(solver_comboBox, Gtk::PACK_SHRINK);
+    pack_start(threading_box, Gtk::PACK_SHRINK);
+    pack_start(theta_box, Gtk::PACK_SHRINK);
 
 
     // Making sure everything is drawn
@@ -47,9 +91,33 @@ SolverCreator::SolverCreator() {
 }
 
 Solver *SolverCreator::createSolver() {
-    return nullptr;
+
+    if (0 == solver_comboBox.get_active_row_number()) {
+        // If Barnes Hut is chosen
+
+        auto solver = new BarnesHutSolver;
+        solver->setTheta(theta_spinButton.get_value());
+        solver->enableThreading(threading_switch.get_active());
+        return solver;
+
+    } else {
+        // If Naive is chosen
+
+        auto solver = new NaiveSolver;
+        solver->enableThreading(threading_switch.get_active());
+        return solver;
+    }
 }
 
 void SolverCreator::on_combo_changed() {
+
+    if (0 == solver_comboBox.get_active_row_number()) {
+        // If Barnes Hut is chosen
+        theta_box.show();
+
+    } else {
+        // If Naive is chosen
+        theta_box.hide();
+    }
 
 }
