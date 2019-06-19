08f868c14ed4582ceeddc8c6e9045aba40cd3dd8
Jackson Campolattaro <jackcamp@vt.edu>
2019-06-19 00:58:35 -0400

    More progress refactoring Body

--- src/n_body/view/rendering/Renderer.h
+++ src/n_body/view/rendering/Renderer.h
@@ -6,8 +6,23 @@
 #define N_BODY_RENDERER_H
 
 
+#include "../../tools/Position.h"
+#include "../../tools/Color.h"
+
 class Renderer {
 
+public:
+
+    /**
+     * Any Renderer will have to know how to draw itself
+     */
+    virtual void draw() = 0;
+
+private:
+
+    Position position;
+
+    Color color;
 };
 
 
