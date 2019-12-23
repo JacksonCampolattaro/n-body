e3cb26bfedcd57685de81eac328ff9bd6c375238
jackcamp <jackcamp@vt.edu>
2019-12-23 00:00:33 -0500

    Added temporary 'runner' methods.

---
+++ src/runner.h
@@ -0,0 +1,24 @@
+//
+// Created by jackcamp on 12/22/19.
+//
+
+#ifndef N_BODY_RUNNER_H
+#define N_BODY_RUNNER_H
+
+
+#include "model/tools/BodyList.h"
+#include "model/PhysicsContext.h"
+
+#include <spdlog/spdlog.h>
+
+namespace runner {
+
+    int headless(BodyList bodies, PhysicsContext physicsContext, unsigned int cycles);
+
+    // TODO
+    int gui();
+    int video();
+};
+
+
+#endif //N_BODY_RUNNER_H
