1bc6da2a18eeb160f192f06eb31aa124b34106b2
JacksonCampolattaro <jackcamp@vt.edu>
2019-12-31 10:50:51 -0500

    Implemented Draw method for the coloredDrawable (from example).

--- src/view/ColoredDrawable.cpp
+++ src/view/ColoredDrawable.cpp
@@ -5,5 +5,13 @@
 #include "ColoredDrawable.h"
 
 void ColoredDrawable::draw(const Matrix4 &transformationMatrix, SceneGraph::Camera3D &camera) {
+    _shader
+            .setDiffuseColor(_color)
+            .setLightPosition(camera.cameraMatrix().transformPoint({-3.0f, 10.0f, 10.0f}))
+            .setTransformationMatrix(transformationMatrix)
+            ///.setNormalMatrix(transformationMatrix.normalMatrix())
+            .setNormalMatrix(transformationMatrix.rotationNormalized())
+            .setProjectionMatrix(camera.projectionMatrix());
 
+    _mesh.draw(_shader);
 }
