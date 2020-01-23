5e9d54f7a980d36d569e772076d84beab89fbc15
jackcamp <jackcamp@vt.edu>
2020-01-22 23:13:28 -0500

    Added View::Application, which will extend Gtk's Application class.

---
+++ src/View/Application.h
@@ -0,0 +1,19 @@
+//
+// Created by jackcamp on 1/22/20.
+//
+
+#ifndef N_BODY_APPLICATION_H
+#define N_BODY_APPLICATION_H
+
+#include <gtkmm/application.h>
+
+namespace View {
+
+    class Application : public Gtk::Application {
+
+    };
+
+}
+
+
+#endif //N_BODY_APPLICATION_H
