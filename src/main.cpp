386a22c4cd286cab0dd2d3fe62f5bed24705c17f
jackcamp <jackcamp@vt.edu>
2020-01-23 19:46:26 -0500

    Declared the basic methods the Config class will provide.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 
 #include <gtkmm/application.h>
+#include "Controller/Config.h"
 #include <iostream>
 
 int main(int argc, char *argv[]) {
 
+    Controller::Config config{argc, argv};
+    return config.run();
 }
