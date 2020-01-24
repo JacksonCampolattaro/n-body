8040e7f2ff7ad8231854ab3e78b6ad898edd8c08
jackcamp <jackcamp@vt.edu>
2020-01-23 21:36:20 -0500

    CLI integration now closer to intended usage.

--- src/Controller/Config.cpp
+++ src/Controller/Config.cpp
@@ -11,16 +11,23 @@ int Controller::Config::parseCommandLine(int argc, char **argv) {
     // Setting the name and description
     CLI::App CLIApplication("Modular Integrator for N-body Interaction");
 
+    try {
+        (CLIApplication).parse((argc), (argv));
+    } catch(const CLI::ParseError &e) {
+        return (CLIApplication).exit(e);
+    }
+
     return EXIT_SUCCESS;
 }
 
 int Controller::Config::run() {
     // @todo Actually create the program
+    return EXIT_SUCCESS;
 }
 
 int Controller::Config::run(int argc, char** argv) {
 
-    parseCommandLine(argc, argv);
-
+    auto status = parseCommandLine(argc, argv);
+    if (0 != status) return status;
     return run();
 }
