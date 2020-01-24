2e821c12dd7865f754070195b951c806072fe8a5
JacksonCampolattaro <jackcamp@vt.edu>
2020-01-24 13:16:27 -0500

    Enabled setting log level at runtime.

--- src/Controller/Logger.h
+++ src/Controller/Logger.h
@@ -12,8 +12,21 @@
 
 namespace Controller {
 
+    /**
+     * @brief Namespace for free functions related to Logging
+     *
+     * Spdlog provides a singleton logger which can be accessed anywhere, this namespaces
+     * groups functions which configure spdlog's default logger.
+     */
     namespace Logger {
 
+        /**
+         * @brief Configures the global spdlog default logger.
+         *
+         * @param silent Nothing is printed to the console when silent is true.
+         * @param logLevel The level of verbosity to include in the logs.
+         * @param logFilePath The location of a file to write a log to.
+         */
         void prepare(
                 bool silent = false,
                 spdlog::level::level_enum logLevel = spdlog::level::info,
