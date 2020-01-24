24b43a3c698bd47e9baebe92dc1e3ccb558fddd2
jackcamp <jackcamp@vt.edu>
2020-01-23 20:00:58 -0500

    Added documentation to Config class.

--- src/Controller/Config.h
+++ src/Controller/Config.h
@@ -7,12 +7,30 @@
 
 namespace Controller {
 
+    /**
+     * @brief A manager class for the program's operation status.
+     *
+     * This class is responsible for keeping track of the program's state, and
+     * all of its settings. It contains many parameters which are set on creation,
+     * but may be updated as the program runs.
+     */
     class Config {
 
     public:
 
+        /**
+         * @brief Constructor that parses command line arguments to create a configuration.
+         *
+         * @param argc The number of arguments passed to the program.
+         * @param argv An array of strings, representing each argument.
+         */
         Config(int argc, char *argv[]);
 
+        /**
+         * @brief Runs the program based on the Config's settings.
+         *
+         * @return The Exit Code of the program (e.g. 0 for success).
+         */
         int run();
 
     private:
