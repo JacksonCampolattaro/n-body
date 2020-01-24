53bbe7acc59cd5c5776c9127168948a2732f644e
jackcamp <jackcamp@vt.edu>
2020-01-24 00:19:44 -0500

    Began creating a framework for logging.

--- src/Controller/Config.cpp
+++ src/Controller/Config.cpp
@@ -6,9 +6,14 @@
 
 Controller::Config::Config() :
         _commandParser{"Modular Integrator for N-body Interaction", "mini"},
+        _logDistributor{std::make_shared<spdlog::sinks::dist_sink_mt>()},
+        _logger{std::make_shared<spdlog::logger>("logger", _logDistributor)},
         _mode{Config::Mode::INTERACTIVE} {
 
-    _commandParser.add_option("--mode", _mode, "Set the graphics mode of the program")
+    _logDistributor->add_sink(std::make_shared<spdlog::sinks::ansicolor_stdout_sink_mt>());
+    _logger->error("Console added as log sink");
+
+    _commandParser.add_option("-m,--mode", _mode, "Set the graphics mode of the program")
             ->transform(CLI::CheckedTransformer{ModeNameMap, CLI::ignore_case});
 
     // @todo Bind more commands to the parser
@@ -29,8 +34,7 @@ int Controller::Config::run(int argc, char **argv) {
 
 int Controller::Config::run() {
 
-    // @todo Actually create the program
-
+    // Run the program in the appropriate mode
     switch (_mode) {
         case Mode::HEADLESS :
             return runHeadless();
@@ -43,13 +47,16 @@ int Controller::Config::run() {
 }
 
 int Controller::Config::runHeadless() {
+    // @todo Actually create the program
     return 0;
 }
 
 int Controller::Config::runViewer() {
+    // @todo Actually create the program
     return 1;
 }
 
 int Controller::Config::runInteractive() {
+    // @todo Actually create the program
     return 2;
 }
