6d92e9bf5fcd54911428e76dd1f6ece7cb8b409a
jackcamp <jackcamp@vt.edu>
2020-01-26 13:39:28 -0500

    Began experimenting with adding options to the application.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 
 #include "Controller/Controller.h"
+#include "Controller/Application.h"
 
 int main(int argc, char *argv[]) {
 
-    Controller::Config config{};
-    return config.run(argc, argv);
+//    Controller::Config config{};
+//    return config.run(argc, argv);
+    Controller::Application().run(argc, argv);
 }
