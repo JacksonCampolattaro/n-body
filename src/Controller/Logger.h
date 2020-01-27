9032e5f7f7fc9cac9765980772baf7e8febb6d12
jackcamp <jackcamp@vt.edu>
2020-01-26 23:41:22 -0500

    Added some documentation to logging methods.

--- src/Controller/Logger.h
+++ src/Controller/Logger.h
@@ -15,17 +15,41 @@ namespace Controller {
     /**
      * @brief Namespace for free functions related to Logging
      *
-     * Spdlog provides a singleton logger which can be accessed anywhere, this namespaces
+     * Spdlog provides a singleton logger which can be accessed anywhere, this namespace
      * groups functions which configure spdlog's default logger.
      */
     namespace Logger {
 
+        /**
+         * @brief Removes options applied to the logger.
+         *
+         * Removes all sinks from spdlog's default instance, to prevent the creation of duplicate sinks.
+         */
         void reset();
 
+        /**
+         * @brief Attaches the console to the logger.
+         *
+         * The console is provided as a sink for the default logger to write logs to.
+         * Logs written to the console do not include as many details as those written to files.
+         */
         void attachConsole();
 
+        /**
+         * @brief Attaches an output file to the logger.
+         *
+         * A file path is used to create a sink for the default logger to write to.
+         * @todo If a file could not be created, the issue should appear as an error in the log.
+         *
+         * @param filePath The path of the file to be created or added to.
+         */
         void attachFile(std::string &filePath);
 
+        /**
+         * @brief Sets the level of verbosity based on a string.
+         *
+         * @param level The logging verbosity level requested
+         */
         void setVerbosity(std::string &level);
 
     };
