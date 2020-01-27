adf659a72741081a11ec9eea8f845567acdb9cbc
JacksonCampolattaro <jackcamp@vt.edu>
2020-01-27 10:05:07 -0500

    Re-removed build directory from git.

--- doc/html/search/functions_0.js
+++ doc/html/search/functions_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['application',['Application',['../classController_1_1Application.html#ab80e09311c2402c2fa81c70de598f521',1,'Controller::Application']]],
   ['attachconsole',['attachConsole',['../namespaceController_1_1Logger.html#ac59dfc82da6b3803faae8bbab0fba789',1,'Controller::Logger']]],
   ['attachfile',['attachFile',['../namespaceController_1_1Logger.html#a30f74b95beb503c15cbca331bf9e229d',1,'Controller::Logger']]]
 ];
