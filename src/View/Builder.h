1bcc884f7a4fd10c436860d063012288bffaed20
JacksonCampolattaro <jackcamp@vt.edu>
2020-02-03 13:27:10 -0500

    Added my own Builder class, for managing the loading of interface files.

---
+++ src/View/Builder.h
@@ -0,0 +1,17 @@
+//
+// Created by jackcamp on 2/3/20.
+//
+
+#ifndef N_BODY_BUILDER_H
+#define N_BODY_BUILDER_H
+
+#include <gtkmm/builder.h>
+
+namespace View {
+
+    class Builder : public Gtk::Builder {
+
+    };
+}
+
+#endif //N_BODY_BUILDER_H
