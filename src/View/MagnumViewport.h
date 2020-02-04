c8d903beddb98eb361510c9ad08c55cf879ff813
JacksonCampolattaro <jackcamp@vt.edu>
2020-02-04 08:50:59 -0500

    Added MagnumViewport class.

---
+++ src/View/MagnumViewport.h
@@ -0,0 +1,16 @@
+//
+// Created by jackcamp on 2/4/20.
+//
+
+#ifndef N_BODY_MAGNUMVIEWPORT_H
+#define N_BODY_MAGNUMVIEWPORT_H
+
+
+#include <gtkmm/glarea.h>
+
+class MagnumViewport : public Gtk::GLArea {
+
+};
+
+
+#endif //N_BODY_MAGNUMVIEWPORT_H
