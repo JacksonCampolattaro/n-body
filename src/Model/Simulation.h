aad6f575524729fa852555b613f17fc4d0830c5a
jackcamp <jackcamp@vt.edu>
2020-04-17 15:25:13 -0400

    Added empty class "Simulation".

---
+++ src/Model/Simulation.h
@@ -0,0 +1,17 @@
+//
+// Created by jackcamp on 4/17/20.
+//
+
+#ifndef N_BODY_SIMULATION_H
+#define N_BODY_SIMULATION_H
+
+namespace Model {
+
+    class Simulation {
+
+    };
+
+}
+
+
+#endif //N_BODY_SIMULATION_H
