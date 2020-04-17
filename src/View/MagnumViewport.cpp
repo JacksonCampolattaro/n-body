9c6e95fb61fe7ed1841f9e68825e49f1c7186be1
jackcamp <jackcamp@vt.edu>
2020-04-17 19:21:54 -0400

    Added Magnum namespace to MagnumViewport

--- src/View/MagnumViewport.cpp
+++ src/View/MagnumViewport.cpp
@@ -6,7 +6,7 @@
 
 View::MagnumViewport::MagnumViewport() :
     Gtk::GLArea(),
-    _context(Magnum::NoCreate) {
+    _context(NoCreate) {
 
     spdlog::trace("MagnumViewport constructor invoked");
 
@@ -27,16 +27,49 @@ View::MagnumViewport::~MagnumViewport() {}
 
 void View::MagnumViewport::onRealize() {
 
+    spdlog::trace("MagnumViewport onRealize invoked");
+
+    // Make the OpenGL context current, and then configure it
+    make_current();
+    _context.create();
 }
 
 bool View::MagnumViewport::onRender(const Glib::RefPtr<Gdk::GLContext> &context) {
+
+    spdlog::trace("MagnumViewport onRender invoked");
+
+    // Reset state, so Gtkmm's defaults won't interfere with Magnum
+    GL::Context::current().resetState(GL::Context::State::ExitExternal);
+
+    // Get the ID of the current framebuffer
+    GLint framebufferID;
+    glGetIntegerv(GL_FRAMEBUFFER_BINDING, &framebufferID);
+
+    // Attach Magnum's framebuffer manager to the framebuffer provided by Gtkmm
+    auto framebuffer =
+            GL::Framebuffer::wrap(framebufferID, {{}, {get_width(), get_height()}});
+
+    // Clear
+    framebuffer.clear(GL::FramebufferClear::Color);
+
+    // Rendering
+    // TODO
+
+    // Undo Magnum's changes to the graphics state
+    Magnum::GL::Context::current().resetState(GL::Context::State::EnterExternal);;
     return false;
 }
 
 void View::MagnumViewport::onResize(int width, int height) {
 
+    spdlog::trace("MagnumViewport onResize invoked");
+
+    // TODO
 }
 
 void View::MagnumViewport::onUnrealize() {
 
+    spdlog::trace("MagnumViewport onUnrealize invoked");
+
+    // TODO
 }
