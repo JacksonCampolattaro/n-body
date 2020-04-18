b6e4f482fee66564d95db2250e591081c652fd57
jackcamp <jackcamp@vt.edu>
2020-04-17 22:12:01 -0400

    Beginning to outline the Simulation class.

--- src/Model/Simulation.h
+++ src/Model/Simulation.h
@@ -6,14 +6,34 @@
 #define N_BODY_SIMULATION_H
 
 #include "Position.h"
+#include "Velocity.h"
+#include "Entity.h"
 
 #include <vector>
+#include <memory>
 
 namespace Model {
 
     class Simulation {
 
+    public:
+
+
+        /**
+         * @brief Adds a new entity to the simulation
+         *
+         * Aspects of the entity are added to their respective collections.
+         * The entity's references to those aspects are reassigned to their new locations.
+         *
+         * @param entity The entity to be added
+         */
+        void addEntity(std::shared_ptr<Entity> entity);
+
+    private:
+
+        std::vector<Entity> _entities;
         std::vector<Position> _positions;
+        std::vector<Velocity> _velocities;
     };
 
 }
