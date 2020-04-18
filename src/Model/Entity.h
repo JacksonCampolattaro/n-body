d4f64adba7be6013e318aba905a381c35891755a
jackcamp <jackcamp@vt.edu>
2020-04-17 21:58:55 -0400

    Added Entity class stub.

---
+++ src/Model/Entity.h
@@ -0,0 +1,17 @@
+//
+// Created by jackcamp on 4/17/20.
+//
+
+#ifndef N_BODY_ENTITY_H
+#define N_BODY_ENTITY_H
+
+namespace Model {
+
+    class Entity {
+
+    };
+
+}
+
+
+#endif //N_BODY_ENTITY_H
