d019dbc0b2c6245f4d266f39689881995d1dd7cf
jackcamp <jackcamp@vt.edu>
2020-04-17 22:12:01 -0400

    Beginning to outline the Simulation class.

--- src/Model/Entity.h
+++ src/Model/Entity.h
@@ -5,10 +5,27 @@
 #ifndef N_BODY_ENTITY_H
 #define N_BODY_ENTITY_H
 
+#include "Position.h"
+#include "Velocity.h"
+
+#include <memory>
+
 namespace Model {
 
+    /**
+     * @brief A class tying together the aspects that describe an object.
+     *
+     * TODO
+     *
+     */
     class Entity {
 
+    public:
+
+    private:
+
+        std::shared_ptr<Position> _position;
+        std::shared_ptr<Velocity> _velocity;
     };
 
 }
