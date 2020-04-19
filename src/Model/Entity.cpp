c26332e7b925fba6d7b8ff65d4a5c18f32f05870
jackcamp <jackcamp@vt.edu>
2020-04-18 23:28:52 -0400

    Added coverage to test compilation.

--- src/Model/Entity.cpp
+++ src/Model/Entity.cpp
@@ -4,6 +4,8 @@
 
 #include "Entity.h"
 
+Model::Entity::Entity() {}
+
 Model::Entity *Model::Entity::setPosition(Model::Position position) {
     _position = std::make_shared<Model::Position>(position);
     return this;
@@ -13,3 +15,25 @@ Model::Entity *Model::Entity::setVelocity(Model::Velocity velocity) {
     _velocity = std::make_shared<Model::Velocity>(velocity);
     return this;
 }
+
+std::string Model::Entity::toString() {
+
+    std::stringstream stream;
+
+    stream << "[ entity: ";
+
+    if (nullptr != _position) {
+        stream << _position->toString();
+    }
+
+    stream << " ";
+
+    if (nullptr != _velocity) {
+        stream << _velocity->toString();
+    }
+
+    stream << " ]";
+
+    return stream.str();
+}
+
