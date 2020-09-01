4f6f9b07e8c0c1ee484653eeacc69998901bbbc0
Jackson Campolattaro <jackcamp@vt.edu>
2020-09-01 17:38:45 -0400

    Add class HeadlessView, implements View

---
+++ src/View/HeadlessView.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jackcamp on 9/1/20.
+//
+
+#include "HeadlessView.h"
---
+++ src/View/HeadlessView.h
@@ -0,0 +1,18 @@
+//
+// Created by jackcamp on 9/1/20.
+//
+
+#ifndef N_BODY_HEADLESSVIEW_H
+#define N_BODY_HEADLESSVIEW_H
+
+#include "View.h"
+
+namespace View {
+
+    class HeadlessView : View {
+
+    };
+
+}
+
+#endif //N_BODY_HEADLESSVIEW_H
