c71075f5c27987ed2f505c3a99dd448d743878e8
Jackson Campolattaro <jackcamp@vt.edu>
2020-09-01 17:46:28 -0400

    Add classes InteractiveView and ViewerView

---
+++ src/View/InteractiveView.h
@@ -0,0 +1,19 @@
+//
+// Created by jackcamp on 9/1/20.
+//
+
+#ifndef N_BODY_INTERACTIVEVIEW_H
+#define N_BODY_INTERACTIVEVIEW_H
+
+#include "View.h"
+
+namespace View {
+
+    class InteractiveView : View {
+
+    };
+
+}
+
+
+#endif //N_BODY_INTERACTIVEVIEW_H
---
+++ src/View/ViewerView.h
@@ -0,0 +1,19 @@
+//
+// Created by jackcamp on 9/1/20.
+//
+
+#ifndef N_BODY_VIEWERVIEW_H
+#define N_BODY_VIEWERVIEW_H
+
+#include "View.h"
+
+namespace View {
+
+    class ViewerView : View {
+
+    };
+
+}
+
+
+#endif //N_BODY_VIEWERVIEW_H
