91089e1db7df8cef7d01dadb337721c363a6fe10
Jackson Campolattaro <jackcamp@vt.edu>
2020-09-01 17:31:45 -0400

    Add abstract View, remove HeadlessController

---
+++ src/View/View.h
@@ -0,0 +1,20 @@
+//
+// Created by jackcamp on 9/1/20.
+//
+
+#ifndef N_BODY_VIEW_H
+#define N_BODY_VIEW_H
+
+namespace View {
+
+    class View {
+
+    public:
+
+
+    };
+
+}
+
+
+#endif //N_BODY_VIEW_H
