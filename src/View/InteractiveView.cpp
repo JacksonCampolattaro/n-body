2ce87f51d9e0ee95fc25724a7adff675086bb9e9
Jackson Campolattaro <jackcamp@vt.edu>
2020-09-01 20:46:05 -0400

    Implement a minimal example of an InteractiveView

--- src/View/InteractiveView.cpp
+++ src/View/InteractiveView.cpp
@@ -6,18 +6,36 @@
 
 #include "../Controller/Application.h"
 
-View::InteractiveView::InteractiveView() {}
+View::InteractiveView::InteractiveView() :
+        _window(),
+        _box(Gtk::Orientation::ORIENTATION_VERTICAL),
+        _view(),
+        _button("text") {
+
+}
 
 void View::InteractiveView::attach_application(Controller::Application *application) {
 
     // Create a window
     spdlog::debug("Creating a window with a viewport");
-    auto window = new ViewerWindow();
+
+    _window.add(_box);
+
+    _button.set_margin_top(5);
+    _button.set_margin_bottom(5);
+    _button.set_margin_left(5);
+    _button.set_margin_right(5);
+
+    _box.pack_start(_view);
+    _box.pack_start(_button, false, true);
 
     // Attach the window to this application
     spdlog::debug("Adding window to application");
-    application->add_window(*window);
-    window->show();
+    application->add_window(_window);
+    _window.show();
+    _box.show();
+    _view.show();
+    _button.show();
 
     // Run the program itself
     spdlog::debug("Running the program");
