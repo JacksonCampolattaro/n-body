ae5f77e46544b164df053aff6feb7e21d70a0a8b
Jackson Campolattaro <jackcamp@vt.edu>
2020-09-03 10:23:29 -0400

    Add a private Entity constructor, with Simulation as a friend class

--- src/Model/Entity.cpp
+++ src/Model/Entity.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "Entity.h"
+
+#include "Simulation.h"
+
+Model::Entity::Entity(Simulation &simulation, const Position &position, const Velocity &velocity) :
+        _simulation(simulation),
+        _position{_simulation._positions, position},
+        _velocity{_simulation._velocities, velocity} {
+
+}
