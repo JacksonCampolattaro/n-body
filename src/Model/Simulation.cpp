59f2d147f214bad5c20596f73fcb80150d0cb1a8
Jackson Campolattaro <jackcamp@vt.edu>
2020-09-02 23:33:54 -0400

    Add ActiveElement class

--- src/Model/Simulation.cpp
+++ src/Model/Simulation.cpp
@@ -30,13 +30,17 @@ void Model::Simulation::addEntity(Model::Position position, Model::Velocity velo
 
 void Model::Simulation::addEntity(Model::Position position, Model::Velocity velocity,
                                   const Model::Drawable::Drawable &drawable,
-                                  const Model::Physics::PassiveElement &passiveElement) {
+                                  float mass) {
 
     addEntity(position, velocity, drawable);
 
-    _passiveElements.push_back(passiveElement);
+    _passiveElements.emplace_back(mass);
     _passiveElements.back()._positionVector = &_positions;
     _passiveElements.back()._positionIndex = _positions.size() - 1;
     _passiveElements.back()._velocityVector = &_velocities;
     _passiveElements.back()._velocityIndex = _velocities.size() - 1;
+
+    _activeElements.emplace_back(mass);
+    _activeElements.back()._positionVector = &_positions;
+    _activeElements.back()._positionIndex = _positions.size() - 1;
 }
