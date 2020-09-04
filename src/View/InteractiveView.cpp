784ed518df22fd38e0fb2bc91f906273918aff1d
Jackson Campolattaro <jackcamp@vt.edu>
2020-09-04 11:49:36 -0400

    Add ability to switch back and forth between frame and continous mode

--- src/View/InteractiveView.cpp
+++ src/View/InteractiveView.cpp
@@ -8,6 +8,7 @@
 
 View::InteractiveView::InteractiveView(Controller::Application &application,
                                        std::shared_ptr<Model::Simulation> simulation) :
+        _simulation(*simulation),
         _window(),
         _vbox(Gtk::Orientation::ORIENTATION_VERTICAL),
         _hbox(Gtk::Orientation::ORIENTATION_HORIZONTAL),
@@ -18,9 +19,10 @@ View::InteractiveView::InteractiveView(Controller::Application &application,
     _window.set_default_size(1000, 1000);
     _window.add(_vbox);
 
-    _button_advance.signal_clicked().connect(sigc::mem_fun(simulation.get(), &Model::Simulation::update));
+    _button_advance.signal_clicked().connect(sigc::mem_fun(this, &InteractiveView::on_advance_clicked));
     simulation->signal_update_complete.connect(sigc::mem_fun(&_viewport, &SimulationViewport::draw));
-    _viewport.draw(simulation->_drawables);
+
+    _button_run.signal_clicked().connect(sigc::mem_fun(this, &InteractiveView::on_run_clicked));
 
     _vbox.pack_start(_viewport);
 
@@ -38,4 +40,16 @@ View::InteractiveView::InteractiveView(Controller::Application &application,
     _button_run.show();
 
     application.add_window(_window);
+}
+
+void View::InteractiveView::on_advance_clicked() {
+
+    _viewport.signal_render_complete.clear();
+    _simulation.update();
+}
+
+void View::InteractiveView::on_run_clicked() {
+
+    _viewport.signal_render_complete.connect(sigc::mem_fun(&_simulation, &Model::Simulation::update));
+    _simulation.update();
 }
