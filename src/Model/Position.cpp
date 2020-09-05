729934c7cd414329dd793a33258bc7751a4dab96
Jackson Campolattaro <jackcamp@vt.edu>
2020-09-05 17:19:11 -0400

    Move find_package calls to top level CMakeLists.txt

--- src/Model/Position.cpp
+++ src/Model/Position.cpp
@@ -8,12 +8,19 @@ std::string Model::Position::toString() const {
     return "(" + std::to_string(this->x) + ", " + std::to_string(this->y) + ", " + std::to_string(this->z) + ")";
 }
 
-std::ostream &Model::operator<<(std::ostream &os, const Model::Position &body) {
+std::ostream &Model::operator<<(std::ostream &os, const Model::Position &position) {
 
     os << "( "
-       << body.x << ", "
-       << body.y << ", "
-       << body.z << " )";
+       << position.x << ", "
+       << position.y << ", "
+       << position.z << " )";
 
     return os;
+}
+
+std::istream &Model::operator>>(std::istream &in, Model::Position &position) {
+
+    char _;
+    in >> _ >> position.x >> _ >> position.y >> _ >> position.z >> _;
+    return in;
 }
