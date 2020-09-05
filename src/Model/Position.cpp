cf1076b522ccbe9661e42a3c4fddb71f64ecfab8
Jackson Campolattaro <jackcamp@vt.edu>
2020-09-05 17:30:26 -0400

    Add ability to save and load positions using the stream operator

--- src/Model/Position.cpp
+++ src/Model/Position.cpp
@@ -7,20 +7,3 @@
 std::string Model::Position::toString() const {
     return "(" + std::to_string(this->x) + ", " + std::to_string(this->y) + ", " + std::to_string(this->z) + ")";
 }
-
-std::ostream &Model::operator<<(std::ostream &os, const Model::Position &position) {
-
-    os << "( "
-       << position.x << ", "
-       << position.y << ", "
-       << position.z << " )";
-
-    return os;
-}
-
-std::istream &Model::operator>>(std::istream &in, Model::Position &position) {
-
-    char _;
-    in >> _ >> position.x >> _ >> position.y >> _ >> position.z >> _;
-    return in;
-}
