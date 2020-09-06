fd0681fef95fd04e7cb4c38cc1afdd3c8b5ccd20
Jackson Campolattaro <jackcamp@vt.edu>
2020-09-06 12:12:45 -0400

    Add the ability to save and load simulations to files

--- src/Model/Simulation.cpp
+++ src/Model/Simulation.cpp
@@ -35,30 +35,43 @@ Model::Entity &Model::Simulation::newEntity() {
 
 bool Model::Simulation::loadBodiesFromPath(const std::string &path) {
 
+    spdlog::trace("Loading file");
+
     std::ifstream file(path);
+    if (!file)
+        spdlog::error("Failed to load file at path: {}", path);
+
     std::stringstream stream;
     stream << file.rdbuf();
     file.close();
 
    rapidjson::Document document;
-   document.Parse(path.c_str());
-   if (document.HasParseError())
+   document.Parse(stream.str().c_str());
+   if (document.HasParseError()) {
+       spdlog::error("Failed to parse file");
        return false;
+   }
 
    document >> *this;
 
+   signal_num_drawables_changed.emit(_drawables);
    return true;
 }
 
 bool Model::Simulation::saveBodiesToPath(const std::string &path) {
 
+    spdlog::trace("Loading file");
+
     std::ofstream file(path);
+    if (!file)
+        spdlog::error("Failed to create file at path: {}", path);
 
     rapidjson::StringBuffer stream;
     rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(stream);
     writer << *this;
     file << stream.GetString();
 
+    file.flush();
     file.close();
     return true;
 }
