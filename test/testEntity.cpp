7a4af1200f2cb25f26b89f2893731e8e8e5c9485
Jackson Campolattaro <jackcamp@vt.edu>
2020-09-06 01:33:40 -0400

    Begin adding ability to serialize and print entity

--- test/testEntity.cpp
+++ test/testEntity.cpp
@@ -3,10 +3,16 @@
 #include "../src/Model/Simulation.h"
 #include "../src/Model/Entity.h"
 
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/prettywriter.h>
+
 using Model::Simulation;
 using Model::Entity;
 
 int main() {
+
+    rapidjson::StringBuffer buffer;
 
     Simulation simulation{};
 
@@ -16,5 +22,13 @@ int main() {
             .setDrawable({{0.1, 0.2, 0.3}, 100})
             .setPassiveElement({200})
             .setActiveElement({300});
+    std::cout << original << std::endl;
+    rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(buffer);
+    writer << original;
+
+    std::string saved = buffer.GetString();
+    std::cout << "archive: \n" << saved << std::endl;
+
+    auto copy = simulation.newEntity();
 
 }
