6ea04efadbd17331c1a230d820892d04a8a7f966
Jackson Campolattaro <jackcamp@vt.edu>
2020-09-06 00:42:44 -0400

    Add ability to serialize and deserialize PassiveElements

--- test/testPassiveElement.cpp
+++ test/testPassiveElement.cpp
@@ -1,25 +1,36 @@
-#include <cassert>
 
 #include "../src/Model/Physics/PassiveElement.h"
+
+#include <cassert>
+#include <iostream>
+
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
 
 using Model::Physics::PassiveElement;
 
 int main() {
 
-    std::stringstream stream;
+    rapidjson::StringBuffer buffer;
 
     // Save the original to a stream
-    PassiveElement original(100);
-    stream << original;
-    std::cout << original;
+    PassiveElement original(500);
+    std::cout << "original: " << original << std::endl;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    writer << original;
+
+    std::string saved = buffer.GetString();
+    std::cout << "archive: " << saved << std::endl;
 
     // Load a copy from the stream
-    PassiveElement copy(1);
-    stream >> copy;
-    std::cout << copy;
+    PassiveElement copy(0);
+    rapidjson::Document document;
+    document.Parse(saved.c_str());
+    document >> copy;
+    std::cout << "copy: " << copy << std::endl;
 
     // Make sure the two match
-    // TODO
+    assert(original == copy);
 
     return 0;
 }
