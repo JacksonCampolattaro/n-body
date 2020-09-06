947c1542e4d290f360d100a17425aebe48158912
Jackson Campolattaro <jackcamp@vt.edu>
2020-09-06 01:14:32 -0400

    Add ability to serialize and print drawables

--- test/testDrawable.cpp
+++ test/testDrawable.cpp
@@ -1,25 +1,35 @@
-#include <cassert>
-
 #include "../src/Model/Drawable/Drawable.h"
+
+#include <cassert>
+#include <iostream>
+
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
 
 using Model::Drawable::Drawable;
 
 int main() {
 
-    std::stringstream stream;
+    rapidjson::StringBuffer buffer;
 
     // Save the original to a stream
-    Drawable original({0.1, 0.2, 0.3}, 100);
-    stream << original;
-    std::cout << original;
+    Drawable original({0.1, 0.2, 0.3}, 4);
+    std::cout << "original: " << original << std::endl;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    writer << original;
+
+    std::string saved = buffer.GetString();
+    std::cout << "archive: " << saved << std::endl;
 
     // Load a copy from the stream
-    Drawable copy({0, 0, 0}, 1);
-    stream >> copy;
-    std::cout << copy;
+    Drawable copy({0, 0, 0}, 0);
+    rapidjson::Document document;
+    document.Parse(saved.c_str());
+    document >> copy;
+    std::cout << "copy: " << copy << std::endl;
 
     // Make sure the two match
-    // TODO
+    assert(original == copy);
 
     return 0;
 }
