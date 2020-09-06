7f59a4f19d9c42039cffb32bf778a6207a5eeae6
Jackson Campolattaro <jackcamp@vt.edu>
2020-09-06 00:50:06 -0400

    Remove Position and Velocity cpp files

--- src/Model/Drawable/Drawable.h
+++ src/Model/Drawable/Drawable.h
@@ -28,100 +28,31 @@ namespace Model {
 
             const Position *_position;
 
-            friend std::ostream &operator<<(std::ostream &os, const Drawable &drawable) {
-
-                os << "{ \"drawable\" : ";
+            template<typename Writer>
+            friend Writer &operator<<(Writer &writer, const Drawable &drawable) {
+                writer.StartObject();
                 {
-
-                    os << "{ \"color\" : ";
-                    {
-                        os << "\""
-                           << drawable._color.x() << " "
-                           << drawable._color.y() << " "
-                           << drawable._color.z()
-                           << "\" ";
-                    }
-                    os << "} ";
-
-                    os << "{ \"radius\" : ";
-                    {
-                        os << drawable._radius << " ";
-                    }
-                    os << "} ";
+                    writer.String("radius");
+                    writer.Double(drawable._radius);
                 }
-                os << "} ";
+                writer.EndObject();
+            }
 
+            friend const rapidjson::Value &operator>>(const rapidjson::Value &obj, Drawable &drawable) {
+
+                drawable._radius = obj["radius"].GetFloat();
+
+                return obj;
+            }
+
+            friend std::ostream &operator<<(std::ostream &os, const Drawable &drawable) {
+                os << "* " << drawable._radius << "r " << " *";
                 return os;
             }
 
-            friend std::istream &operator>>(std::istream &in, Drawable &drawable) {
-
-                std::string _;
-
-                // The first value should be the open bracket
-                in >> _;
-                assert("{" == _);
-                {
-
-                    // The next value should be the name
-                    in >> std::quoted(_);
-                    assert("drawable" == _);
-
-                    // The next value should be the colon
-                    in >> _;
-                    assert(":" == _);
-
-                    // TODO: Split this out
-                    // The first value of the color should be the open bracket
-                    in >> _;
-                    assert("{" == _);
-                    {
-
-                        // The next value should be the name
-                        in >> std::quoted(_);
-                        assert("color" == _);
-
-                        // The next value should be the colon
-                        in >> _;
-                        assert(":" == _);
-
-                        // Get the value
-                        std::string value;
-                        in >> std::quoted(value);
-                        std::stringstream(value) >> drawable._color.x() >> drawable._color.y() >> drawable._color.z();
-
-                    }
-                    // The last value should be the close bracket
-                    in >> _;
-                    assert("}" == _);
-
-                    // TODO: Split this out
-                    // The first value of the radius should be the open bracket
-                    in >> _;
-                    assert("{" == _);
-                    {
-
-                        // The next value should be the name
-                        in >> std::quoted(_);
-                        assert("radius" == _);
-
-                        // The next value should be the colon
-                        in >> _;
-                        assert(":" == _);
-
-                        // Get the value
-                        in >> drawable._radius;
-
-                    }
-                    // The last value should be the close bracket
-                    in >> _;
-                    assert("}" == _);
-                }
-                // The last value should be the close bracket
-                in >> _;
-                assert("}" == _);
-
-                return in;
+            friend bool operator==(const Drawable &left, const Drawable &right) {
+                // TODO: This could be improved
+                return left._radius == right._radius && left._color == right._color;
             }
         };
 
--- src/Model/Velocity.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by jackcamp on 4/17/20.
-//
-
-#include "Velocity.h"
