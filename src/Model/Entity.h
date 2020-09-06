cfa0f600933109d19643f09162c29b1b5f14af83
Jackson Campolattaro <jackcamp@vt.edu>
2020-09-06 01:33:40 -0400

    Begin adding ability to serialize and print entity

--- src/Model/Entity.h
+++ src/Model/Entity.h
@@ -28,6 +28,40 @@ namespace Model {
 
         Entity &setActiveElement(const Physics::ActiveElement &activeElement);
 
+        template<typename Writer>
+        friend Writer &operator<<(Writer &writer, const Entity &entity) {
+            writer.StartObject();
+            {
+                writer.String("position");
+                writer << *entity._position;
+
+                writer.String("velocity");
+                writer << *entity._velocity;
+            }
+            writer.EndObject();
+        }
+
+        friend const rapidjson::Value &operator>>(const rapidjson::Value &obj, Entity &entity) {
+
+            Position position(0, 0, 0);
+            obj >> position;
+            entity.setPosition(position);
+
+            Velocity velocity(0, 0, 0);
+            obj >> velocity;
+            entity.setVelocity(velocity);
+
+            return obj;
+        }
+
+        friend std::ostream &operator<<(std::ostream &os, const Entity &entity) {
+            os << "{ " << *entity._position << " " << *entity._velocity << " }";
+            return os;
+        }
+
+        friend bool operator==(const Entity &left, const Entity &right) {
+            return *left._position == *right._position && *left._velocity == *right._velocity;
+        }
     private:
 
         Entity(Simulation &simulation);
