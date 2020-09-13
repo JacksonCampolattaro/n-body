b300838ffa065bcf80584b5ed01adf7ef262931f
Jackson Campolattaro <jackcamp@vt.edu>
2020-09-12 21:49:40 -0400

    Add a painter as a member of the Interactive viewer

--- src/View/Painter.cpp
+++ src/View/Painter.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "Painter.h"
+
+View::Painter::Painter() {
+
+}
+
+void View::Painter::set_glarea(Gtk::GLArea &glArea) {
+
+}
+
+void View::Painter::draw(const std::deque<Model::Drawable::Drawable> &drawables) {
+
+}
+
+void View::Painter::on_realize() {
+
+}
+
+bool View::Painter::on_render(const Glib::RefPtr<Gdk::GLContext> &context) {
+    return false;
+}
+
+void View::Painter::on_resize(int width, int height) {
+
+}
+
+void View::Painter::on_unrealize() {
+
+}
