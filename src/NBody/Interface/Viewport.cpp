d594be18f85e73eb1c21a08d14fdfa9dbfba6de5
Jackson Campolattaro <jackcamp@vt.edu>
2020-12-22 00:13:20 -0500

    Use new Viewport class rather than separate window and view

--- src/NBody/Interface/Viewport.cpp
+++ src/NBody/Interface/Viewport.cpp
@@ -7,6 +7,10 @@
 Interface::Viewport::Viewport() {
 
     _window.add(_view);
+    _window.show();
+    _view.show();
+
+    slot_setDrawables = sigc::mem_fun(&_view, &View::View::setDrawables);
 }
 
 Gtk::Window &Interface::Viewport::window() {
