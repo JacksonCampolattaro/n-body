fb4c023fa9cf14c110d8f1d73a8ad6eb843f3e5b
Jackson Campolattaro <jackcamp@vt.edu>
2020-12-22 12:08:55 -0500

    Add namespace to internal libraries

--- src/NBody/Simulation/Simulation.h
+++ src/NBody/Simulation/Simulation.h
@@ -5,10 +5,16 @@
 #ifndef N_BODY_SIMULATION_H
 #define N_BODY_SIMULATION_H
 
+#include <entt/entity/registry.hpp>
+
 namespace NBody::Simulation {
 
     class Simulation {
+    public:
 
+    private:
+
+        entt::registry _registry;
     };
 
 }
