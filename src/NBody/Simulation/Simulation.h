7da4b7464e6a98ebbbc1741597a12780057eb89a
Jackson Campolattaro <jackcamp@vt.edu>
2020-12-22 11:53:22 -0500

    Add a new Simulation directory, and include EnTT using FetchContent

---
+++ src/NBody/Simulation/Simulation.h
@@ -0,0 +1,16 @@
+//
+// Created by jackcamp on 12/22/20.
+//
+
+#ifndef N_BODY_SIMULATION_H
+#define N_BODY_SIMULATION_H
+
+namespace NBody::Simulation {
+
+    class Simulation {
+
+    };
+
+}
+
+#endif //N_BODY_SIMULATION_H
