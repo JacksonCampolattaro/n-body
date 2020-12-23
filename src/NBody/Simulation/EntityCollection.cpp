521551f5d08a3a4e153927becebd33d82be73a4b
Jackson Campolattaro <jackcamp@vt.edu>
2020-12-23 00:14:01 -0500

    EntityCollection subclasses entt registry to enable serialization

---
+++ src/NBody/Simulation/EntityCollection.cpp
@@ -0,0 +1,13 @@
+//
+// Created by jackcamp on 12/22/20.
+//
+
+#include "EntityCollection.h"
+
+void NBody::Simulation::to_json(json &j, const NBody::Simulation::EntityCollection &entityCollection) {
+
+}
+
+void NBody::Simulation::from_json(const json &j, NBody::Simulation::EntityCollection &entityCollection) {
+
+}
---
+++ src/NBody/Simulation/EntityCollection.h
@@ -0,0 +1,22 @@
+//
+// Created by jackcamp on 12/22/20.
+//
+
+#ifndef N_BODY_ENTITYCOLLECTION_H
+#define N_BODY_ENTITYCOLLECTION_H
+
+#include <nlohmann/json.hpp>
+#include <entt/entity/registry.hpp>
+
+using nlohmann::json;
+
+namespace NBody::Simulation {
+
+    class EntityCollection : public entt::registry {};
+
+    void to_json(json &j, const EntityCollection &entityCollection);
+
+    void from_json(const json &j, EntityCollection &entityCollection);
+}
+
+#endif //N_BODY_ENTITYCOLLECTION_H
