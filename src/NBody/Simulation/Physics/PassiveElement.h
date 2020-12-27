da388d6795b5c76987dfcb600ecc38373e0cc7c3
Jackson Campolattaro <jackcamp@vt.edu>
2020-12-27 01:49:01 -0500

    Mostly implement Physics::PassiveElement

---
+++ src/NBody/Simulation/Physics/PassiveElement.h
@@ -0,0 +1,35 @@
+//
+// Created by jackcamp on 9/2/20.
+//
+
+#ifndef N_BODY_PASSIVEELEMENT_H
+#define N_BODY_PASSIVEELEMENT_H
+
+#include <NBody/Simulation/Position.h>
+#include <NBody/Simulation/Velocity.h>
+
+namespace NBody::Simulation::Physics {
+
+    class PassiveElement {
+    public:
+
+        explicit PassiveElement(float mass);
+
+        [[nodiscard]] float &mass();
+        [[nodiscard]] const float &mass() const;
+
+    private:
+
+        float _mass;
+    };
+
+    bool operator==(const PassiveElement &left, const PassiveElement &right);
+
+    void to_json(json &j, const PassiveElement &passiveElement);
+
+    void from_json(const json &j, PassiveElement &passiveElement);
+
+}
+
+
+#endif //N_BODY_PASSIVEELEMENT_H
