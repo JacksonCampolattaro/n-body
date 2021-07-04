02b0e1fbe0b3d4e386af5ea09281a67dfefd89f5
jackcamp <jackcamp@vt.edu>
2021-07-04 15:05:29 +0200

    Remove .idea file and documentation compilation artifacts

--- doc/html/search/functions_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['reset',['reset',['../namespaceController_1_1Logger.html#af6dad87d5575525f3ab749ead10da772',1,'Controller::Logger']]],
-  ['run',['run',['../classController_1_1Config.html#ac37d92603bcd7a4f27e64b92a9cff1c0',1,'Controller::Config::run(int argc, char **argv)'],['../classController_1_1Config.html#adcbaa24721e3f3db508c93301b75a49c',1,'Controller::Config::run()']]]
-];
--- doc/html/search/namespaces_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['logger',['Logger',['../namespaceController_1_1Logger.html',1,'Controller']]]
-];
