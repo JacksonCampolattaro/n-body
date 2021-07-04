3ab15493951df3631bec87b1fb4a39087d324740
jackcamp <jackcamp@vt.edu>
2021-07-04 23:11:04 +0200

    Add a couple of simple unit tests for serialization

--- src/NBody/Physics/Position.cpp
+++ src/NBody/Physics/Position.cpp
@@ -3,19 +3,3 @@
 //
 
 #include "Position.h"
-
-void NBody::Simulation::to_json(json &j, const NBody::Simulation::Position &position) {
-
-    j = {
-            {"x", position.x},
-            {"y", position.y},
-            {"z", position.z}
-    };
-}
-
-void NBody::Simulation::from_json(const json &j, NBody::Simulation::Position &position) {
-
-    j.at("x").get_to(position.x);
-    j.at("y").get_to(position.y);
-    j.at("z").get_to(position.z);
-}
