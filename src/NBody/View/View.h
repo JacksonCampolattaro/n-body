176a3006f4702a3a08424ff14d49e4f987e2af18
jackcamp <jackcamp@vt.edu>
2021-07-18 14:44:56 +0200

    Use Magnum instead of raw OpenGL calls to set background color

--- src/NBody/View/View.h
+++ src/NBody/View/View.h
@@ -7,10 +7,21 @@
 
 #include <gtkmm/glarea.h>
 
+#include <Magnum/Platform/GLContext.h>
+#include <Magnum/Math/Color.h>
+
+#include <Magnum/GL/Framebuffer.h>
+#include <Magnum/GL/Renderer.h>
+
+using namespace Magnum;
+using namespace Math::Literals;
+
 namespace NBody {
 
     class View : public Gtk::GLArea {
     private:
+
+        Platform::GLContext _context{NoCreate, 0, nullptr};
 
     public:
 
@@ -18,15 +29,25 @@ namespace NBody {
 
             set_auto_render();
 
+            set_required_version(4, 5);
+
             signal_realize().connect(sigc::mem_fun(this, &View::onRealize));
             signal_unrealize().connect(sigc::mem_fun(this, &View::onUnrealize));
             signal_render().connect(sigc::mem_fun(this, &View::onRender));
         };
 
         void onRealize() {
-            make_current();
 
-            glClearColor(0.9f, 1.0f, 1.0f, 1.0f);
+            // Initialize the GL context
+            make_current();
+            _context.create();
+
+            // Configure the graphics features
+            GL::Renderer::enable(GL::Renderer::Feature::DepthTest);
+            GL::Renderer::enable(GL::Renderer::Feature::FaceCulling);
+
+            // Set the background color
+            GL::Renderer::setClearColor(Math::Color4<float>::red());
         }
 
         void onUnrealize() {
@@ -34,7 +55,27 @@ namespace NBody {
         }
 
         bool onRender(const Glib::RefPtr<Gdk::GLContext> &context) {
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            // Eliminate outside effects on GL state
+            GL::Context::current().resetState(GL::Context::State::ExitExternal);
+
+            // Determine the relevant framebuffer by getting its ID
+            GLint framebufferID;
+            glGetIntegerv(GL_FRAMEBUFFER_BINDING, &framebufferID);
+
+            // Attach Magnum's framebuffer manager to the framebuffer provided by Gtkmm
+            auto gtkmmDefaultFramebuffer = GL::Framebuffer::wrap(
+                    framebufferID,
+                    {{},{get_width(), get_height()}}
+            );
+
+            // Reset color and depth buffers
+            gtkmmDefaultFramebuffer.clear(GL::FramebufferClear::Color | GL::FramebufferClear::Depth);
+
+            // TODO: This is where actual rendering code goes!
+
+            // Restore external GL state
+            GL::Context::current().resetState(GL::Context::State::ExitExternal);
             return false;
         }
 
