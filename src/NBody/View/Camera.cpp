0b99456d74a8581ddeb4315751d552ed6cd6c211
jackcamp <jackcamp@vt.edu>
2021-08-12 22:04:10 +0200

    Simplify camera class, removing unused member variables

--- src/NBody/View/Camera.cpp
+++ src/NBody/View/Camera.cpp
@@ -4,12 +4,15 @@
 
 #include "Camera.h"
 
+#include <entt/entity/view.hpp>
+
 using namespace Magnum::Math::Literals;
 
 void NBody::Camera::draw(const NBody::Simulation::Simulation &simulation) {
 
     auto view = simulation.view<const NBody::Physics::Position, const NBody::Graphics::Color, const NBody::Graphics::Sphere>();
     auto mesh = Graphics::Sphere::mesh();
+
     view.each([&](const NBody::Physics::Position &position, const NBody::Graphics::Color &color,
                   const NBody::Graphics::Sphere &sphere) {
 
@@ -29,17 +32,33 @@ void NBody::Camera::draw(const NBody::Simulation::Simulation &simulation) {
 
 }
 
+//void NBody::Camera::draw(const entt::basic_view<const NBody::Physics::Position, const NBody::Graphics::Color, const NBody::Graphics::Sphere> &view) {
+//
+//    auto mesh = Graphics::Sphere::mesh();
+//    view.each([&](const NBody::Physics::Position &position, const NBody::Graphics::Color &color,
+//                  const NBody::Graphics::Sphere &sphere) {
+//
+//        auto transformationMatrix =
+//                Matrix4::translation({position.x, position.y, position.z}) *
+//                Matrix4::scaling(Vector3{sphere.radius()});
+//
+//        Shaders::PhongGL shader{{}, 1};
+//        shader.setLightPositions({{24.0f, 50.0f, -50, 0.0f}})
+//                .setDiffuseColor(color)
+//                .setAmbientColor(Color3::fromHsv({color.hue(), 1.0f, 0.5f}))
+//                .setTransformationMatrix(transformationMatrix)
+//                .setNormalMatrix(transformationMatrix.normalMatrix())
+//                .setProjectionMatrix(_projection)
+//                .draw(mesh);
+//    });
+//}
+
 void NBody::Camera::setAspectRatio(float aspectRatio) {
 
     _projection = Matrix4{
             Matrix4::perspectiveProjection(35.0_degf, aspectRatio, 0.01f, 1000.0f) *
             Matrix4::translation(Vector3::zAxis(-50.0f))
     };
-}
-
-void NBody::Camera::draw(
-        const entt::view<const NBody::Physics::Position, const NBody::Graphics::Color, const NBody::Graphics::Sphere> &view) {
-
 }
 
 //void NBody::Camera::draw(const NBody::Simulation::Simulation &simulation) {
