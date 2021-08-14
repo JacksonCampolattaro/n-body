4cc225fa2f99500b05b3b84bbb910b0ac23fed48
jackcamp <jackcamp@vt.edu>
2021-08-15 01:25:14 +0200

    Split functions into declarations and implementations

--- src/Application/Interface/Interactive.h
+++ src/Application/Interface/Interactive.h
@@ -32,25 +32,7 @@ namespace Interface {
 
     public:
 
-        Interactive(Simulation &simulation) : Interface(simulation),
-                                              _view(simulation),
-                                              _configurationPanel(simulation),
-                                              _paned(Gtk::Orientation::ORIENTATION_HORIZONTAL) {
-
-            add(_paned);
-            _paned.show();
-
-            _paned.pack1(_view, true, false);
-            _view.show();
-
-            _paned.pack2(_configurationPanel, false, false);
-            _configurationPanel.show();
-
-
-            signal_key_press_event().connect(sigc::mem_fun(*this, &Interactive::on_keyPressEvent), false);
-
-            signal_moveCamera.connect(_view.slot_moveCamera);
-        }
+        Interactive(Simulation &simulation);
 
     public:
         // Signals
@@ -60,34 +42,7 @@ namespace Interface {
     public:
         // Slots
 
-        bool on_keyPressEvent(GdkEventKey *event) {
-
-            float stepSize = 2.0;
-
-            if (event->keyval == GDK_KEY_Left) {
-                signal_moveCamera.emit(Matrix4::translation(Vector3::xAxis(stepSize)));
-            }
-
-            if (event->keyval == GDK_KEY_Right) {
-                signal_moveCamera.emit(Matrix4::translation(Vector3::xAxis(-stepSize)));
-            }
-
-            if (event->keyval == GDK_KEY_Up) {
-                if (event->state & GDK_CONTROL_MASK)
-                    signal_moveCamera.emit(Matrix4::translation(Vector3::yAxis(-stepSize)));
-                else
-                    signal_moveCamera.emit(Matrix4::translation(Vector3::zAxis(stepSize)));
-            }
-
-            if (event->keyval == GDK_KEY_Down) {
-                if (event->state & GDK_CONTROL_MASK)
-                    signal_moveCamera.emit(Matrix4::translation(Vector3::yAxis(stepSize)));
-                else
-                    signal_moveCamera.emit(Matrix4::translation(Vector3::zAxis(-stepSize)));
-            }
-
-            return false;
-        }
+        bool on_keyPressEvent(GdkEventKey *event);
 
     };
 }
--- src/Application/Interface/Simple.cpp
+++ src/Application/Interface/Simple.cpp
@@ -3,3 +3,8 @@
 //
 
 #include "Simple.h"
+
+Interface::Simple::Simple(Simulation &simulation) : Interface(simulation), _view(simulation) {
+    add(_view);
+    _view.show();
+}
