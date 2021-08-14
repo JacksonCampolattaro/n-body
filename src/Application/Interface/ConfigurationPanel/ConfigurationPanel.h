444b6e1116c566cb8c9347d8f9a8d67ed0c9251e
jackcamp <jackcamp@vt.edu>
2021-08-14 19:26:26 +0200

    Add more placeholder tabs, each with their own icon

--- src/Application/Interface/ConfigurationPanel/ConfigurationPanel.h
+++ src/Application/Interface/ConfigurationPanel/ConfigurationPanel.h
@@ -14,9 +14,21 @@ namespace Interface {
     class ConfigurationPanel : public Gtk::Notebook {
     private:
 
-        Gtk::Image _cameraIcon;
+        Gtk::Image
+                _bodiesIcon,
+                _physicsIcon,
+                _solverIcon,
+                _positionIcon,
+                _recordingIcon,
+                _runIcon;
 
-        Gtk::Label _placeholder1, _placeholder2;
+        Gtk::Label
+                _bodiesPlaceholder{"bodies"},
+                _physicsPlaceholder{"physics"},
+                _solverPlaceholder{"solver"},
+                _positionPlaceholder{"position"},
+                _recordingPlaceholder{"recording"},
+                _runPlaceholder{"run"};
 
     public:
 
@@ -24,13 +36,29 @@ namespace Interface {
 
             set_tab_pos(Gtk::POS_LEFT);
 
-            append_page(_placeholder1, "1");
-            _placeholder1.show();
+            append_page(_bodiesPlaceholder, _bodiesIcon);
+            _bodiesIcon.set_from_resource("/NBody/icons/outliner_ob_pointcloud.svg");
+            _bodiesPlaceholder.show();
 
-            append_page(_placeholder2, _cameraIcon);
-            _cameraIcon.set_from_resource("/NBody/icons/view_camera.svg");
-            _cameraIcon.show();
-            _placeholder2.show();
+            append_page(_physicsPlaceholder, _physicsIcon);
+            _physicsIcon.set_from_resource("/NBody/icons/physics.svg");
+            _physicsPlaceholder.show();
+
+            append_page(_solverPlaceholder, _solverIcon);
+            _solverIcon.set_from_resource("/NBody/icons/memory.svg");
+            _solverPlaceholder.show();
+
+            append_page(_positionPlaceholder, _positionIcon);
+            _positionIcon.set_from_resource("/NBody/icons/orientation_global.svg");
+            _positionPlaceholder.show();
+
+            append_page(_recordingPlaceholder, _recordingIcon);
+            _recordingIcon.set_from_resource("/NBody/icons/view_camera.svg");
+            _recordingPlaceholder.show();
+
+            append_page(_runPlaceholder, _runIcon);
+            _runIcon.set_from_resource("/NBody/icons/tria_right.svg");
+            _runPlaceholder.show();
         }
 
     public:
