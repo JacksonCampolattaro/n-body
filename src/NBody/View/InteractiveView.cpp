95a59d2c6ad242d6d29c61e581c55478c3eb2182
jackcamp <jackcamp@vt.edu>
2021-08-18 23:59:37 +0200

    Add a simple scene meant for testing

--- src/NBody/View/InteractiveView.cpp
+++ src/NBody/View/InteractiveView.cpp
@@ -4,3 +4,21 @@
 
 #include "InteractiveView.h"
 
+NBody::InteractiveView::InteractiveView(NBody::GtkmmArcBallCamera &camera, NBody::Simulation &simulation) :
+        View(camera, simulation) {
+
+    signal_button_press_event().connect(camera.slot_buttonPress);
+    signal_button_release_event().connect(camera.slot_buttonRelease);
+
+    add_events(
+            Gdk::BUTTON_PRESS_MASK |
+            Gdk::BUTTON1_MOTION_MASK |
+            Gdk::BUTTON2_MOTION_MASK
+    );
+    signal_motion_notify_event().connect(camera.slot_mouseMotion);
+
+    add_events(
+            Gdk::SMOOTH_SCROLL_MASK
+    );
+    signal_scroll_event().connect(camera.slot_scroll);
+}
