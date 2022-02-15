2e4124408b26897204233848457fa574ca0ee470
JacksonCampolattaro <jackcamp@vt.edu>
2022-02-15 23:32:23 +0100

    Add a new directory for development of a gtkmm4 ui, update FindGtkmm to support gtkmm4

--- modules/FindGtkmm.cmake
+++ modules/FindGtkmm.cmake
@@ -1,5 +1,5 @@
 
 find_package(PkgConfig)
-pkg_check_modules(GTKMM gtkmm-3.0)
+pkg_check_modules(GTKMM gtkmm-4.0)
 link_directories(${GTKMM_LIBRARY_DIRS})
 include_directories(${GTKMM_INCLUDE_DIRS})
