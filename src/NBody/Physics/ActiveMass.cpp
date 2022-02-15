ddb942df61b6673fde64f527de5c8dd77b7cd201
JacksonCampolattaro <jackcamp@vt.edu>
2022-02-15 21:35:03 +0100

    Add unit tests for serialization and deserialization

--- src/NBody/Physics/ActiveMass.cpp
+++ src/NBody/Physics/ActiveMass.cpp
@@ -4,6 +4,10 @@
 
 #include "ActiveMass.h"
 
+bool NBody::Physics::ActiveMass::operator==(const NBody::Physics::ActiveMass &other) const {
+    return mass() == other.mass();
+}
+
 void NBody::Physics::to_json(json &j, const NBody::Physics::ActiveMass &m) {
     j = m.mass();
 }
