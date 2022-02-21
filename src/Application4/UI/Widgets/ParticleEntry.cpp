238e68484fdbc002c5f29d86e2f7dca940fbf37d
JacksonCampolattaro <jackcamp@vt.edu>
2022-02-21 12:18:37 +0100

    Add a simple preview widget for spheres

--- src/Application4/UI/Widgets/ParticleEntry.cpp
+++ src/Application4/UI/Widgets/ParticleEntry.cpp
@@ -5,37 +5,55 @@
 #include "ParticleEntry.h"
 
 
-UI::ParticleEntry::ParticleEntry() : Gtk::Grid() {
+UI::ParticleEntry::ParticleEntry() : Gtk::Box(Gtk::Orientation::HORIZONTAL) {
 
-    set_row_spacing(4);
-    set_column_spacing(8);
+    set_spacing(10);
 
-    _icon.set(Gdk::Pixbuf::create_from_resource("/NBody/icons/node_material.svg", -1, 256));
-    _icon.set_expand();
-    attach(_icon, 0, 0, 1, 2);
+    _grid.set_row_spacing(4);
+    _grid.set_column_spacing(8);
+
+    //_icon.set(Gdk::Pixbuf::create_from_resource("/NBody/icons/node_material.svg", -1, 256));
+    //_icon.set_expand();
+    //attach(_icon, 0, 0, 1, 2);
+    //_sphereView.set_expand();
+    append(_sphereView);
 
     _positionLabel.add_css_class("description");
     _velocityLabel.add_css_class("description");
     _massLabel.add_css_class("description");
-    attach(_positionLabel, 1, 0);
-    attach(_velocityLabel, 1, 1);
-    attach(_massLabel, 3, 0);
+    _grid.attach(_positionLabel, 0, 0);
+    _grid.attach(_velocityLabel, 0, 1);
+    _grid.attach(_massLabel, 2, 0);
 
-    attach(_positionView, 2, 0);
-    attach(_velocityView, 2, 1);
-    attach(_massView, 4, 0);
+    _grid.attach(_positionView, 1, 0);
+    _grid.attach(_velocityView, 1, 1);
+    _grid.attach(_massView, 3, 0);
 
-
-//    append(_positionLabel);
-//
-//    append(_positionEntry);
-//
-//    append(_velocityLabel);
-//
-//    append(_massLabel);
+    append(_grid);
 }
 
 void UI::ParticleEntry::bind(const NBody::Simulation::Particle &particle) {
 
+    if (particle.all_of<NBody::Graphics::Color, NBody::Graphics::Sphere>()) {
+
+        auto color = particle.get<NBody::Graphics::Color>();
+        auto sphere = particle.get<NBody::Graphics::Sphere>();
+        _sphereView.setValue(color, sphere);
+    }
+
     auto position = particle.get<NBody::Physics::Position>();
+    _positionView.setValue(position);
+
+    auto velocity = particle.get<NBody::Physics::Velocity>();
+    _velocityView.setValue(velocity);
+
+    if (particle.all_of<NBody::Physics::ActiveMass>()) {
+        _massLabel.show();
+        _massView.show();
+        auto mass = particle.get<NBody::Physics::ActiveMass>();
+        _massView.set_text(std::to_string(mass.mass()));
+    } else {
+        _massLabel.hide();
+        _massView.hide();
+    }
 }
