498c0926424d1bce75f7b1611ee17ce4217549ec
JacksonCampolattaro <jackcamp@vt.edu>
2022-06-20 11:59:37 +0200

    Use squared distance when calculating force of gravity

--- src/NBody/Physics/Rule.h
+++ src/NBody/Physics/Rule.h
@@ -7,6 +7,7 @@
 
 #include <glm/vec3.hpp>
 #include <glm/geometric.hpp>
+#include <glm/gtx/norm.hpp>
 
 #include "Position.h"
 #include "ActiveMass.h"
@@ -24,12 +25,13 @@ namespace NBody::Physics {
 
     public:
 
-        explicit Rule(const float &G = 1.0, const float &epsilon = 0.0000001) : _g(G), _epsilon(epsilon) {};
+        explicit Rule(const float &G = 1.0, const float &epsilon = 0.00001) : _g(G), _epsilon(epsilon) {};
 
         Acceleration operator()(const Position &activePosition, const ActiveMass &activeMass,
                                 const Position &passivePosition, const PassiveMass &passiveMass) const {
+
             float force = _g * passiveMass.mass() * activeMass.mass() /
-                          (glm::length(activePosition - passivePosition) + _epsilon);
+                          (glm::distance2((glm::vec3) activePosition, (glm::vec3) passivePosition) + _epsilon);
 
             glm::vec3 forceVector = glm::normalize(activePosition - passivePosition) * force;
 
@@ -38,7 +40,7 @@ namespace NBody::Physics {
 
         float &g() { return _g; }
 
-        float &epsilong() { return _epsilon; }
+        float &epsilon() { return _epsilon; }
     };
 
 }
