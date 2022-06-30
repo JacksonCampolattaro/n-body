729981364b6a49ca2a56f6e9812969433a126915
JacksonCampolattaro <jackcamp@vt.edu>
2022-06-30 13:16:58 +0200

    Clean up particles panel UI and add a (non-functional) file chooser dialogue

--- src/Application/UI/Sidebar/ParticlesPanel.cpp
+++ src/Application/UI/Sidebar/ParticlesPanel.cpp
@@ -3,12 +3,14 @@
 //
 
 #include <spdlog/spdlog.h>
-#include <gtkmm/button.h>
+#include <gtkmm/applicationwindow.h>
+
 #include "ParticlesPanel.h"
 
 UI::ParticlesPanel::ParticlesPanel(NBody::Simulation &simulation) :
         Panel("Particles"),
-        _particlesView(simulation) {
+        _particlesView(simulation),
+        _fileChooser("Choose a Scenario") {
 
     auto builder = Gtk::Builder::create_from_resource("/ui/particles_panel.xml");
     auto root = builder->get_widget<Gtk::Widget>("root");
@@ -31,4 +33,28 @@ UI::ParticlesPanel::ParticlesPanel(NBody::Simulation &simulation) :
             },
             false
     );
+
+    _contents.set_expand();
+    append(*builder->get_widget<Gtk::ActionBar>("action-bar"));
+
+    auto saveButton = builder->get_widget<Gtk::Button>("save-button");
+    saveButton->signal_clicked().connect([&] {
+        spdlog::debug("save clicked");
+    });
+
+    auto openButton = builder->get_widget<Gtk::Button>("open-button");
+    openButton->signal_clicked().connect([&] {
+        spdlog::debug("open clicked");
+        _fileChooser.show();
+    });
+
+
+    // The file chooser should always be displayed over the main window
+    // the main window appears last in the list of top level widgets
+    auto toplevels = Gtk::Window::list_toplevels();
+    _fileChooser.set_transient_for(*toplevels[toplevels.size() - 1]);
+    _fileChooser.set_modal(true);
+
+    _fileChooser.add_button("_Cancel", Gtk::ResponseType::CANCEL);
+    _fileChooser.add_button("_Open", Gtk::ResponseType::OK);
 }
