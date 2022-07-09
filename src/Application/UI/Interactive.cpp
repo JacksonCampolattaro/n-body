fdc5b90f3d98905a44220cb5b43567a8402f82bc
JacksonCampolattaro <jackcamp@vt.edu>
2022-07-09 20:26:03 +0200

    Automatically switch background color when launched in light mode

--- src/Application/UI/Interactive.cpp
+++ src/Application/UI/Interactive.cpp
@@ -5,6 +5,7 @@
 #include "Interactive.h"
 
 #include <gtkmm/binlayout.h>
+#include <adwaita.h>
 
 UI::Interactive::Interactive(Gtk::ApplicationWindow::BaseObjectType *cobject, const Glib::RefPtr<Gtk::Builder> &builder,
                              NBody::Simulation &simulation, NBody::Physics::Rule &rule, NBody::Solver &solver) :
@@ -33,8 +34,14 @@ UI::Interactive::Interactive(Gtk::ApplicationWindow::BaseObjectType *cobject, co
     flap.get_preferred_size(min, natural);
     flap.set_size_request(natural.get_width(), natural.get_height());
 
-    _view.signal_doneRendering.connect([&](){
+    _view.signal_doneRendering.connect([&]() {
         spdlog::trace("done rendering");
     });
+
+    if (adw_style_manager_get_dark(adw_style_manager_get_default())) {
+        _camera.setBackgroundColor({0.12, 0.12, 0.1, 1.0});
+    } else {
+        _camera.setBackgroundColor({0.9, 0.9, 0.92, 1.0});
+    }
 
 }
