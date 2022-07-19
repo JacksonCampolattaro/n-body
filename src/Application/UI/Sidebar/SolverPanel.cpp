df5a8a6d78880cf3bbc7a356a33e04dadb68b5fc
JacksonCampolattaro <jackcamp@vt.edu>
2022-07-19 10:08:55 +0200

    Switch between algorithms using Adwaita ComboRow

--- src/Application/UI/Sidebar/SolverPanel.cpp
+++ src/Application/UI/Sidebar/SolverPanel.cpp
@@ -5,24 +5,35 @@
 #include "SolverPanel.h"
 
 #include <NBody/Simulation/Solvers/BarnesHutSolver.h>
+#include <adwaita.h>
 
 UI::SolverPanel::SolverPanel(NBody::MultiSolver &multiSolver) :
         Panel("Solver"),
         _multiSolver(multiSolver),
         _builder(Gtk::Builder::create_from_resource("/ui/solver_panel.xml")),
-        _placeholder(*_builder->get_widget<Gtk::Button>("placeholder")) {
+        _solverDropdown(*_builder->get_widget<Gtk::ListBoxRow>("solver-dropdown")),
+        _solverStack(*_builder->get_widget<Gtk::Stack>("stack")) {
 
     auto root = _builder->get_widget<Gtk::Widget>("root");
     _contents.append(*root);
 
     _multiSolver.select<NBody::NaiveSolver>();
-    _placeholder.signal_clicked().connect([&] {
-        if (_placeholder.get_label() == "Naive") {
-            _placeholder.set_label("Barnes Hut");
-            _multiSolver.select<NBody::BarnesHutSolver>();
-        } else {
-            _placeholder.set_label("Naive");
-            _multiSolver.select<NBody::NaiveSolver>();
+    _solverStack.set_visible_child("naive");
+    _solverDropdown.connect_property_changed("selected", [&]() {
+        auto algorithm = _solverDropdown.get_property<guint>("selected");
+        _solverDropdown.set_property("selected", algorithm);
+        switch (algorithm) {
+            case 0:
+                _multiSolver.select<NBody::NaiveSolver>();
+                _solverStack.set_visible_child("naive");
+                break;
+            case 1:
+                _multiSolver.select<NBody::BarnesHutSolver>();
+                _solverStack.set_visible_child("barnes-hut");
+                break;
+            default:
+                spdlog::error("Unrecognized algorithm selected");
+                break;
         }
     });
 }
