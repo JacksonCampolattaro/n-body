eabc7e4d9c8669adb6888825750971fcbbd42204
JacksonCampolattaro <jackcamp@vt.edu>
2022-07-26 22:42:09 +0200

    Add custom UI for different run-modes, including selectable step count

--- src/Application/UI/Sidebar/RunPanel.cpp
+++ src/Application/UI/Sidebar/RunPanel.cpp
@@ -10,12 +10,24 @@
 
 #include "RunPanel.h"
 
+static std::string validate(const std::string &in) {
+    auto filtered = in;
+    std::erase_if(filtered, [](const auto &c) {
+        return !std::isdigit(c);
+    });
+    return filtered.empty() ? "1" : filtered;
+}
+
 UI::RunPanel::RunPanel(NBody::Solver &solver) :
         Panel("Run"),
         _solver(solver),
         _builder(Gtk::Builder::create_from_resource("/ui/run_panel.xml")),
         _runModeDropdown(*_builder->get_widget<Gtk::ListBoxRow>("run-mode-dropdown")),
-        _runButton(*_builder->get_widget<Gtk::ToggleButton>("run-button")),
+        _runContinuouslyButton(*_builder->get_widget<Gtk::ToggleButton>("run-continuously-button")),
+        _runOneStepButton(*_builder->get_widget<Gtk::ToggleButton>("run-one-step-button")),
+        _runNStepsButton(*_builder->get_widget<Gtk::ToggleButton>("run-n-steps-button")),
+        _stepCountEntry(*_builder->get_widget<Gtk::Entry>("step-count-entry")),
+        _runControllerStack(*_builder->get_widget<Gtk::Stack>("run-controller-stack")),
         _iterationsLabel(*_builder->get_widget<Gtk::Label>("iterations-label")),
         _stepTimeLabel(
                 *Gtk::Builder::get_widget_derived<PreciseFloatView>(_builder, "step-time-label")
@@ -29,10 +41,44 @@ UI::RunPanel::RunPanel(NBody::Solver &solver) :
     _contents.set_expand();
     append(*_builder->get_widget<Gtk::ActionBar>("action-bar"));
 
-    _runButton.signal_toggled().connect([&] {
-        if (_runButton.get_active())
-            run();
+
+    _runControllerStack.set_visible_child("continuous");
+    _runModeDropdown.connect_property_changed("selected", [&]() {
+        switch (_runModeDropdown.get_property<guint>("selected")) {
+            case 0:
+                _runControllerStack.set_visible_child("continuous");
+                break;
+            case 1:
+                _runControllerStack.set_visible_child("one-step");
+                break;
+            case 2:
+                _runControllerStack.set_visible_child("n-steps");
+                break;
+            default:
+                spdlog::error("Unrecognized run mode selected");
+                break;
+        }
+    });
+
+    _runContinuouslyButton.signal_toggled().connect([&] {
+        if (_runContinuouslyButton.get_active())
+            run(std::numeric_limits<std::size_t>::max());
         else
+            stop();
+    });
+
+    _runOneStepButton.signal_toggled().connect([&] {
+        if (_runOneStepButton.get_active())
+            run(1);
+        else
+            stop();
+    });
+
+    _runNStepsButton.signal_toggled().connect([&] {
+        if (_runNStepsButton.get_active()) {
+            _stepCountEntry.set_text(validate(_stepCountEntry.get_text()));
+            run(std::stoi(_stepCountEntry.get_text()));
+        } else
             stop();
     });
 
@@ -54,28 +100,16 @@ UI::RunPanel::RunPanel(NBody::Solver &solver) :
 
 }
 
-void UI::RunPanel::run() {
-
-    switch (_runModeDropdown.get_property<guint>("selected")) {
-        case 0:
-            run(std::numeric_limits<std::size_t>::max());
-            break;
-        case 1:
-            run(100);
-            break;
-        case 2:
-            run(1);
-            break;
-        default:;
-            spdlog::error("Unrecognized algorithm selected");
-            break;
-    }
-}
-
 void UI::RunPanel::run(std::size_t n) {
+    spdlog::debug("Running {} steps", n);
+
     _currentIterations = 1;
     _idler = Glib::signal_timeout().connect([this, n] {
+
         _solver.slot_step()();
+
+        _stepCountEntry.set_progress_fraction((double) _currentIterations / (double) n);
+
         if (_currentIterations >= n)
             stop();
         return true;
@@ -84,5 +118,8 @@ void UI::RunPanel::run(std::size_t n) {
 
 void UI::RunPanel::stop() {
     _idler.disconnect();
-    _runButton.set_active(false);
+    _runContinuouslyButton.set_active(false);
+    _runOneStepButton.set_active(false);
+    _runNStepsButton.set_active(false);
+    _stepCountEntry.set_progress_fraction(0.0);
 }
