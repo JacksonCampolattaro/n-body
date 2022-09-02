e06765971362bc2d58a818edd15a6ed1cf289b0d
JacksonCampolattaro <jackcamp@vt.edu>
2022-09-02 13:37:08 +0200

    Replace custom modules directory with modern use of PkgConfig

--- modules/FindGio.cmake
+++
@@ -1,5 +0,0 @@
-
-find_package(PkgConfig)
-pkg_check_modules(GIO gio-2.0)
-link_directories(${GIO_LIBRARY_DIRS})
-include_directories(${GIO_INCLUDE_DIRS})
--- modules/FindGtkmm.cmake
+++
@@ -1,5 +0,0 @@
-
-find_package(PkgConfig)
-pkg_check_modules(GTKMM gtkmm-4.0)
-link_directories(${GTKMM_LIBRARY_DIRS})
-include_directories(${GTKMM_INCLUDE_DIRS})
