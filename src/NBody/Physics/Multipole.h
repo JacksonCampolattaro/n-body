41fb3a2683d8d1a8fe90d819ce9c830a8509680c
JacksonCampolattaro <jackcamp@vt.edu>
2023-02-27 15:38:12 +0100

    Add a basic multipole type with no useful features

---
+++ src/NBody/Physics/Multipole.h
@@ -0,0 +1,116 @@
+//
+// Created by Jackson Campolattaro on 2/27/23.
+//
+
+#ifndef N_BODY_MULTIPOLE_H
+#define N_BODY_MULTIPOLE_H
+
+#include <array>
+#include <span>
+
+#include <glm/vec3.hpp>
+
+namespace NBody {
+
+    template<int Order>
+    class Multipole : protected Multipole<Order - 1> {
+    public:
+
+        static constexpr std::size_t DataSize = Multipole<Order - 1>::DataSize + (Order + 1);
+
+    protected:
+
+        std::array<float, DataSize> _data;
+
+    public:
+
+        Multipole() :
+                Multipole<Order - 1>(), _data({0}) {}
+
+        explicit Multipole(std::array<float, DataSize> values) :
+                Multipole<Order - 1>(), _data(values) {}
+
+        using Multipole<Order - 1>::get;
+
+        template<std::size_t... Indices>
+        float &get() {
+            return _data[underlyingIndex<Indices...>()];
+        }
+
+        template<std::size_t... Indices>
+        [[nodiscard]] const float &get() const {
+            return _data[underlyingIndex<Indices...>()];
+        }
+
+    protected:
+
+template<std::size_t... Indices>
+static constexpr std::array<std::size_t, Order> orderedIndices() {
+
+    // Make sure the user passed the right number of indices
+    static_assert(sizeof...(Indices) == Order, "Incorrect number of indices for multipole order");
+
+    // Put the indices in XYZ order, to ensure we're accessing the wrong hyper-pyramid of the symmetric matrix
+    std::array<std::size_t, Order> array{Indices...};
+    std::sort(array.begin(), array.end());
+    return array;
+}
+
+        template<std::array<std::size_t, Order> Indices>
+        static constexpr std::array<std::size_t, Order - 1> lowerOrderIndices() {
+            std::array<std::size_t, Order - 1> copy;
+            std::copy(Indices.begin() + 1, Indices.end(), copy.begin());
+            return copy;
+        }
+
+        template<std::array<std::size_t, Order> Indices>
+        static constexpr std::size_t underlyingIndex() {
+
+            // If the first index is X, then we can use lower-dimensional indexing
+            if (Indices[0] == 0) {
+                constexpr auto copy = lowerOrderIndices<Indices>();
+                return Multipole<Order - 1>::template underlyingIndex<copy>();
+            }
+
+            // All the extra linear indices will correspond to indices which contain only Ys and Zs
+            // We can determine the offset by counting the Zs
+            constexpr std::size_t numberOfZs = std::count(Indices.begin(), Indices.end(), 2);
+            return Multipole<Order - 1>::DataSize + numberOfZs;
+        }
+
+        template<std::size_t... Indices>
+        static constexpr std::size_t underlyingIndex() {
+            constexpr auto indices = orderedIndices<Indices...>();
+            return underlyingIndex<indices>();
+        }
+    };
+
+    template<>
+    class Multipole<1> : public glm::vec3 {
+    public:
+
+        using glm::vec3::vec;
+
+        static constexpr std::size_t DataSize = 3;
+
+        Multipole(std::array<float, DataSize> values) : glm::vec3(values[0], values[1], values[2]) {}
+
+        template<std::array<std::size_t, 1> Indices>
+        float &get() { return operator[](Indices[0]); }
+
+        template<std::array<std::size_t, 1> Indices>
+        [[nodiscard]] const float &get() const { return operator[](Indices[0]); }
+
+    protected:
+
+        template<std::array<std::size_t, 1> Indices>
+        static constexpr std::size_t underlyingIndex() {
+            static_assert(Indices[0] < 3, "Index out of bounds!");
+            return Indices[0];
+        }
+
+    };
+
+}
+
+#endif //N_BODY_MULTIPOLE_H
---
+++ src/NBody/Physics/test/multipole.cpp
@@ -0,0 +1,65 @@
+//
+// Created by Jackson Campolattaro on 2/27/23.
+//
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <catch2/generators/catch_generators_random.hpp>
+#include <catch2/generators/catch_generators_adapters.hpp>
+
+#include <NBody/Physics/Multipole.h>
+
+TEST_CASE("Member access to a Quadrupole should be correct", "[Multipole]") {
+
+    // Construct a quadrupole with known values
+    NBody::Multipole<2> q{
+            {0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f}
+    };
+
+    const std::size_t X = 0;
+    const std::size_t Y = 1;
+    const std::size_t Z = 2;
+
+    REQUIRE(q.get<X, X>() == 0.0f);
+
+    REQUIRE(q.get<X, Y>() == 1.0f);
+    REQUIRE(q.get<Y, X>() == 1.0f);
+
+    REQUIRE(q.get<X, Z>() == 2.0f);
+    REQUIRE(q.get<Z, X>() == 2.0f);
+
+    REQUIRE(q.get<Y, Y>() == 3.0f);
+
+    REQUIRE(q.get<Y, Z>() == 4.0f);
+    REQUIRE(q.get<Z, Y>() == 4.0f);
+
+    REQUIRE(q.get<Z, Z>() == 5.0f);
+}
+
+TEST_CASE("Member access to an Octupole should be correct", "[Multipole]") {
+
+    // Construct a quadrupole with known values
+    NBody::Multipole<3> q{
+            {0.0f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f}
+    };
+
+    const std::size_t X = 0;
+    const std::size_t Y = 1;
+    const std::size_t Z = 2;
+
+    REQUIRE(q.get<X, X, X>() == 0.0f);
+    REQUIRE(q.get<X, X, Y>() == 1.0f);
+    REQUIRE(q.get<X, X, Z>() == 2.0f);
+    REQUIRE(q.get<X, Y, Y>() == 3.0f);
+    REQUIRE(q.get<X, Y, Z>() == 4.0f);
+    REQUIRE(q.get<X, Z, Z>() == 5.0f);
+    REQUIRE(q.get<Y, Y, Y>() == 6.0f);
+    REQUIRE(q.get<Y, Y, Z>() == 7.0f);
+    REQUIRE(q.get<Y, Z, Z>() == 8.0f);
+    REQUIRE(q.get<Z, Z, Z>() == 9.0f);
+}
+
+TEST_CASE("The outer product of two Multipole<1>s is a Multipole<2>", "[Multipole]") {
+
+    // todo
+}
