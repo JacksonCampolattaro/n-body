83afaa7ca80f98dca3fc58432dc761817f9f05bc
JacksonCampolattaro <jackcamp@vt.edu>
2023-06-21 15:33:12 +0200

    Add field collection & plotting for multipoles

--- src/Benchmark/Experiments/sampleMultipoles.cpp
+++ src/Benchmark/Experiments/sampleMultipoles.cpp
@@ -29,63 +29,205 @@ std::vector<glm::vec3> range3D(const BoundingBox &sampleRegion, float sampleReso
     return samples;
 }
 
-Acceleration exactField(const Simulation &simulation, const glm::vec3 &samplePosition) {
-    Gravity rule{};
-    auto activeView = simulation.view<const Position, const Mass>();
-    return std::transform_reduce(
-            activeView.begin(), activeView.end(), Acceleration{}, std::plus<>{},
-            [&](auto e) {
-                return rule(activeView.get<const Position>(e), activeView.get<const Mass>(e), samplePosition);
-            }
-    );
-}
-
-void sampleExactField(const Simulation &simulation, const BoundingBox &sampleRegion, float sampleResolution) {
+void sampleField(const std::string &name, const std::function<Acceleration(Position)> &field,
+                 const BoundingBox &sampleRegion, float sampleResolution) {
 
     std::ofstream out{fmt::format(
-            "benchmarks/sample-exact-{}x{}.csv",
-            sampleRegion.dimensions().x, sampleRegion.dimensions().y
+            "benchmarks/sample-{}.csv", name
     )};
     out << "x,y,z,fx,fy,fz\n";
 
     for (auto position: range3D(sampleRegion, sampleResolution)) {
-        auto field = exactField(simulation, position);
+        auto f = field(position);
         out << fmt::format(
                 "{},{},{},{},{},{}\n",
                 position.x, position.y, position.z,
-                field.x, field.y, field.z
+                f.x, f.y, f.z
         );
     }
 }
 
-template<std::size_t Order>
-void sampleMultipoleMoment() {
+void sampleError(const std::string &name,
+                 const std::function<Acceleration(Position)> &field,
+                 const std::function<Acceleration(Position)> &exactField,
+                 const BoundingBox &sampleRegion, float sampleResolution) {
 
-    std::ofstream out{fmt::format("benchmarks/sample-{}-moment.csv", Multipole<Order>::name())};
+    std::ofstream out{fmt::format(
+            "benchmarks/sample-{}-error.csv", name
+    )};
+    out << "x,y,z,ex,ey,ez,e\n";
 
-    MultipoleMoment<Order> moment;
+    for (auto position: range3D(sampleRegion, sampleResolution)) {
+        auto e = exactField(position) - field(position);
+        out << fmt::format(
+                "{},{},{},{},{},{},{}\n",
+                position.x, position.y, position.z,
+                e.x, e.y, e.z,
+                glm::length(e) / glm::length((glm::vec3) exactField(position))
+        );
+    }
 }
 
-template<std::size_t Order>
-void sampleMultipoleField() {
+void sampleDifference(const std::string &name,
+                      const std::function<Acceleration(Position)> &modulationField,
+                      const std::function<Acceleration(Position)> &baseField,
+                      const BoundingBox &sampleRegion, float sampleResolution) {
 
+    std::ofstream out{fmt::format(
+            "benchmarks/sample-{}.csv", name
+    )};
+    out << "x,y,z,fx,fy,fz\n";
+
+    for (auto position: range3D(sampleRegion, sampleResolution)) {
+        auto f = modulationField(position) - baseField(position);
+        out << fmt::format(
+                "{},{},{},{},{},{}\n",
+                position.x, position.y, position.z,
+                f.x, f.y, f.z
+        );
+    }
 }
 
 int main(int argc, char *argv[]) {
     spdlog::set_level(spdlog::level::info);
 
+    Gravity rule{};
     Simulation trio;
     trio.newParticle().setPosition({0.0f, -2.0f, 0.0f}).setMass(2.0f);
     trio.newParticle().setPosition({-1.0f, 2.0f, 0.0f}).setMass(1.0f);
     trio.newParticle().setPosition({1.0f, 2.0f, 0.0f}).setMass(1.0f);
+    auto activeView = trio.view<const Position, const Mass>();
+    auto activeList = std::vector<Entity>{activeView.begin(), activeView.end()};
 
-    BoundingBox fullRange{{-4, -4, 0},
-                          {4,  4,  0}};
-    float fullRangeResolution = 0.04;
-    BoundingBox zoomRange{{1.5f, -0.5f, 0},
-                          {2.5f, 0.5f, 0}};
+
+    BoundingBox fullRange{{-10, -10, 0},
+                          {10,  10,  0}};
+    float fullRangeResolution = 0.05;
+
+    BoundingBox zoomRange{{4, -1, 0},
+                          {6, 1,  0}};
     float zoomRangeResolution = 0.01;
 
-    sampleExactField(trio, fullRange, fullRangeResolution);
-    sampleExactField(trio, zoomRange, zoomRangeResolution);
+    BoundingBox extremeZoomRange{{4.9, -0.1, 0},
+                          {5.1, 0.1,  0}};
+    float extremeZoomRangeResolution = 0.001;
+
+    BoundingBox extendedRange{{-100, -100, 0},
+                              {100,  100,  0}};
+    float extendedRangeResolution = 0.5;
+
+
+    auto exactField = [&](Position p) -> Acceleration {
+        return std::transform_reduce(
+                activeView.begin(), activeView.end(), Acceleration{}, std::plus<>{},
+                [&](auto e) {
+                    return rule(activeView.get<const Position>(e), activeView.get<const Mass>(e), p);
+                }
+        );
+    };
+    sampleField("exact", exactField, fullRange, fullRangeResolution);
+    sampleField("exact-zoom", exactField, zoomRange, zoomRangeResolution);
+
+    // Moment approximations
+    {
+
+        CenterOfMassSummary monopoleMassSummary;
+        monopoleMassSummary.summarize(activeList, activeView);
+        auto monopoleMomentField = [&](Position p) -> Acceleration {
+            return rule(monopoleMassSummary.centerOfMass(), monopoleMassSummary, p);
+        };
+        sampleField("monopole-moment", monopoleMomentField, fullRange, fullRangeResolution);
+        sampleError("monopole-moment", monopoleMomentField, exactField, fullRange, fullRangeResolution);
+        sampleError("monopole-moment-extended", monopoleMomentField, exactField, extendedRange,
+                    extendedRangeResolution);
+
+        QuadrupoleMassSummary quadrupoleMassSummary;
+        quadrupoleMassSummary.summarize(activeList, activeView);
+        auto quadrupoleMomentField = [&](Position p) -> Acceleration {
+            return rule(quadrupoleMassSummary.centerOfMass(), quadrupoleMassSummary, p);
+        };
+        sampleField("net-quadrupole-moment", quadrupoleMomentField, fullRange, fullRangeResolution);
+        sampleDifference("quadrupole-moment", quadrupoleMomentField, monopoleMomentField, fullRange,
+                         fullRangeResolution);
+        sampleError("quadrupole-moment", quadrupoleMomentField, exactField, fullRange, fullRangeResolution);
+
+        OctupoleMassSummary octupoleMassSummary;
+        octupoleMassSummary.summarize(activeList, activeView);
+        auto octupoleMomentField = [&](Position p) -> Acceleration {
+            return rule(octupoleMassSummary.centerOfMass(), octupoleMassSummary, p);
+        };
+        sampleDifference("octupole-moment", octupoleMomentField, quadrupoleMomentField, fullRange, fullRangeResolution);
+        sampleError("octupole-moment", octupoleMomentField, exactField, fullRange, fullRangeResolution);
+
+
+        HexadecupoleMassSummary hexadecupoleMassSummary;
+        hexadecupoleMassSummary.summarize(activeList, activeView);
+        auto hexadecupoleMomentField = [&](Position p) -> Acceleration {
+            return rule(hexadecupoleMassSummary.centerOfMass(), hexadecupoleMassSummary, p);
+        };
+        sampleDifference("hexadecupole-moment", hexadecupoleMomentField, octupoleMomentField, fullRange,
+                         fullRangeResolution);
+        sampleError("hexadecupole-moment", hexadecupoleMomentField, exactField, fullRange, fullRangeResolution);
+    }
+
+    // Field approximations
+    {
+        auto samplePosition = zoomRange.center();
+
+        glm::vec3 netApproximateAcceleration{};
+        QuadrupoleAccelerationSummary netQuadrupoleAcceleration{};
+        OctupoleAccelerationSummary netOctupoleAcceleration{};
+        HexadecupoleAccelerationSummary netHexadecupoleAcceleration{};
+        TriacontadyupoleAccelerationSummary netTriacontadyupoleAcceleration{};
+        activeView.each([&](const Position &activePosition, const Mass &activeMass) {
+            netApproximateAcceleration += (glm::vec3) rule(activePosition, activeMass, samplePosition);
+            rule(activePosition, activeMass, samplePosition, netQuadrupoleAcceleration);
+            rule(activePosition, activeMass, samplePosition, netOctupoleAcceleration);
+            rule(activePosition, activeMass, samplePosition, netHexadecupoleAcceleration);
+            rule(activePosition, activeMass, samplePosition, netTriacontadyupoleAcceleration);
+        });
+
+        auto approximateFunction = [&](Position p) -> Acceleration {
+            return netApproximateAcceleration;
+        };
+        sampleField("vector-field", approximateFunction, zoomRange, zoomRangeResolution);
+        sampleError("vector-field", approximateFunction, exactField, zoomRange, zoomRangeResolution);
+
+        auto quadrupoleApproximateFunction = [&](Position p) -> Acceleration {
+            return netQuadrupoleAcceleration.acceleration().at(p - samplePosition);
+        };
+        sampleField("net-quadrupole-field", quadrupoleApproximateFunction,
+                    zoomRange, zoomRangeResolution);
+        sampleDifference("quadrupole-field", quadrupoleApproximateFunction, approximateFunction,
+                         zoomRange, zoomRangeResolution);
+        sampleError("quadrupole-field", quadrupoleApproximateFunction, approximateFunction,
+                    zoomRange, zoomRangeResolution);
+        sampleError("quadrupole-field-zoom", quadrupoleApproximateFunction, approximateFunction,
+                    extremeZoomRange, extremeZoomRangeResolution);
+
+        auto octupoleApproximateFunction = [&](Position p) -> Acceleration {
+            return netOctupoleAcceleration.acceleration().at(p - samplePosition);
+        };
+        sampleDifference("octupole-field", octupoleApproximateFunction, quadrupoleApproximateFunction,
+                         zoomRange, zoomRangeResolution);
+        sampleError("octupole-field", octupoleApproximateFunction, quadrupoleApproximateFunction,
+                    zoomRange, zoomRangeResolution);
+
+        auto hexadecupoleApproximateFunction = [&](Position p) -> Acceleration {
+            return netHexadecupoleAcceleration.acceleration().at(p - samplePosition);
+        };
+        sampleDifference("hexadecupole-field", hexadecupoleApproximateFunction, octupoleApproximateFunction,
+                         zoomRange, zoomRangeResolution);
+        sampleError("hexadecupole-field", hexadecupoleApproximateFunction, octupoleApproximateFunction,
+                    zoomRange, zoomRangeResolution);
+
+        auto triacontadyupoleApproximateFunction = [&](Position p) -> Acceleration {
+            return netTriacontadyupoleAcceleration.acceleration().at(p - samplePosition);
+        };
+        sampleDifference("triacontadyupole-field", triacontadyupoleApproximateFunction, hexadecupoleApproximateFunction,
+                         zoomRange, zoomRangeResolution);
+        sampleError("triacontadyupole-field", triacontadyupoleApproximateFunction, hexadecupoleApproximateFunction,
+                    zoomRange, zoomRangeResolution);
+    }
+
 }
--- src/Benchmark/plot.py
+++ src/Benchmark/plot.py
@@ -4,11 +4,14 @@ import os.path
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn
 import seaborn as sns
 import colorsys
 from matplotlib import rcParams
 from matplotlib.patches import Rectangle
 import matplotlib.ticker as plticker
+from mpl_toolkits.mplot3d import Axes3D
 
 # Text-width in inches, to make sure everything fits in the latex document
 text_width = 5
@@ -30,6 +33,51 @@ color_map = {
     'Node-Particle': 'lightseagreen',
     'Node-Node': 'aquamarine'
 }
+error_color_map = 'magma'
+directional_color_map = mpl.colors.LinearSegmentedColormap.from_list(
+    "directional",
+    sns.color_palette("husl", 4) + [sns.color_palette("husl", 4)[0]]
+)
+
+
+def polar_to_rgb(r, th):
+    r = np.cbrt(np.clip(r, 0.0, 1.0))
+    return np.asarray(directional_color_map(th / (2 * np.pi), r))
+
+
+def field_key():
+
+    fig = plt.figure(figsize=(0.5*text_width, 0.5*text_width))
+
+    rad = np.geomspace(0.00001, 100, 100)
+    azm = np.linspace(2 * np.pi / 100, 2 * np.pi, 100)
+    r, th = np.meshgrid(rad, azm)
+    colors = np.vectorize(polar_to_rgb, signature='(),()->(n)')(r / 100, th)
+
+    plt.subplot(projection="polar")
+    plt.pcolormesh(th, r, colors, shading='nearest')
+    plt.plot(azm, r, color='k', ls='none')
+    plt.grid()
+    plt.gca().set_axisbelow(False)
+    plt.gca().set_rscale('symlog')
+    plt.gca().yaxis.set_major_formatter(mpl.ticker.PercentFormatter())
+
+    plt.tight_layout()
+    plt.savefig("benchmarks/field_key.pdf")
+    plt.clf()
+
+
+def field_error_key():
+    fig = plt.figure(figsize=(text_width, text_width / 6))
+    mpl.colorbar.ColorbarBase(fig.gca(), orientation='horizontal',
+                              cmap=error_color_map,
+                              norm=mpl.colors.Normalize(0, 0.5),  # vmax and vmin
+                              extend='max',
+                              format=mpl.ticker.PercentFormatter()
+                              )
+    plt.tight_layout()
+    plt.savefig("benchmarks/error_key.pdf")
+    plt.clf()
 
 
 def plot_field(filename, zoom_box=None):
@@ -41,14 +89,17 @@ def plot_field(filename, zoom_box=None):
     # Convert to a 3d array, where the third axis contains [fx, fy]
     arr = data.set_index(['x', 'y']).to_xarray().to_array().transpose().to_numpy()
 
+
     # Determine the strongest force being shown
     max_magnitude = max([np.linalg.norm(xy) for xy in arr.reshape(-1, 2)])
 
     def to_rgb(xy):
         x, y = xy
-        saturation = max(math.sqrt(np.linalg.norm(xy) / max_magnitude), 0.1)
-        hue = (math.atan2(x, y) + math.pi) / (2 * math.pi)
-        return np.asarray(colorsys.hsv_to_rgb(hue, saturation, 1.0))
+        r = np.log(np.linalg.norm(xy) + 1.0) / np.log(max_magnitude + 1.0)
+        th = (np.arctan2(-x, -y) + math.pi)
+        if r > 1.0:
+            return np.asarray([0.0, 0.0, 0.0, 1.0])
+        return polar_to_rgb(r, th)
 
     extent = (data['x'].min(), data['x'].max(), data['y'].min(), data['y'].max())
 
@@ -80,12 +131,53 @@ def plot_field(filename, zoom_box=None):
 
     plt.tight_layout()
     plt.savefig(os.path.splitext(filename)[0] + ".pdf")
+    plt.clf()
+    plt.cla()
+    return extent
+
+
+def plot_field_error(filename):
+    data = pd.read_csv(filename)
+
+    # Ignore Z for a 2d plot
+    data.drop(columns=['z'], inplace=True)
+
+    # Here, we only care about error magnitudes
+    data.drop(columns=['ex', 'ey', 'ez'], inplace=True)
+
+    # Convert to a 3d array, where the third axis contains [e]
+    err = data.set_index(['x', 'y']).to_xarray().to_array().transpose().to_numpy()
+    err = np.squeeze(err)
+
+    # Convert the error to a percentage
+    err = err * 100
+
+    extent = (data['x'].min(), data['x'].max(), data['y'].min(), data['y'].max())
+
+    plt.figure(figsize=(text_width / 2, text_width / 2))
+    image = plt.imshow(
+        err,
+        vmin=0.0,
+        vmax=0.5,
+        cmap=error_color_map,
+        origin='lower',
+        extent=extent,
+    )
+    plt.gca().xaxis.set_major_locator(plticker.MaxNLocator(nbins=4, min_n_ticks=5))
+    plt.gca().yaxis.set_major_locator(plticker.MaxNLocator(nbins=4, min_n_ticks=5))
+
+    # Maybe we can omit X and Y labels?
+    # plt.xlabel('X', size=9)
+    # plt.ylabel('Y', size=9)
+
+    plt.tight_layout()
+    plt.savefig(os.path.splitext(filename)[0] + ".pdf")
+    plt.clf()
     return extent
 
 
 def plot_sweep_n(filename):
     sweep_n = pd.read_csv(filename)
-    print(sweep_n.to_string())
 
     sns.lineplot(data=sweep_n, x="n", y="Time (s)", hue="Solver")
     plt.show()
@@ -93,7 +185,6 @@ def plot_sweep_n(filename):
 
 def plot_sweep_theta(filename):
     sweep_theta = pd.read_csv(filename)
-    print(sweep_theta.to_string())
 
     sns.lineplot(data=sweep_theta, x="θ", y="Time (s)", hue="Solver")
     plt.show()
@@ -101,7 +192,6 @@ def plot_sweep_theta(filename):
 
 def plot_sweep_error(filename):
     sweep_theta = pd.read_csv(filename)
-    print(sweep_theta)
 
     sns.lineplot(data=sweep_theta, x="% Error (Constitutional)", y="Time (s)", hue="Solver")
     plt.show()
@@ -111,14 +201,12 @@ def plot_interaction_counts(filename):
     df = pd.read_csv(filename)
     x_label = df.columns[0]
     particle_count = df['N'][0]
-    print(particle_count)
     df = df[
         [x_label, 'Particle-Particle', 'Particle-Node', 'Node-Particle', 'Node-Node']
     ]
     df = df.loc[:, (df != 0).any(axis=0)]
     df.loc[:, df.columns != x_label] = \
         100 * df.loc[:, df.columns != x_label] / (particle_count * particle_count)
-    print(df)
 
     ax = df.plot(kind='bar', stacked=True,
                  figsize=(text_width / 2, 3),
@@ -129,13 +217,13 @@ def plot_interaction_counts(filename):
     plt.tight_layout()
     plt.legend(loc='lower left')
     plt.savefig(os.path.splitext(filename)[0] + "_vs_interactions.pdf")
+    plt.clf()
 
 
 def plot_times(filename):
     df = pd.read_csv(filename)
     x_label = df.columns[0]
     df = df[[x_label, 'Time']]
-    print(df)
 
     ax = df.plot(kind='bar', legend=False,
                  figsize=(text_width / 2, 3),
@@ -145,13 +233,13 @@ def plot_times(filename):
 
     plt.tight_layout()
     plt.savefig(os.path.splitext(filename)[0] + "_vs_time.pdf")
+    plt.clf()
 
 
 def plot_times_vs_n(filename):
     df = pd.read_csv(filename)
     x_label = df.columns[0]
     df = df[[x_label, 'N', 'Time']]
-    print(df)
 
     df = df.drop(df[df[x_label] == 'RBH'].index)
 
@@ -163,14 +251,13 @@ def plot_times_vs_n(filename):
 
     plt.tight_layout()
     plt.savefig(os.path.splitext(filename)[0] + "_vs_time.pdf")
-    plt.cla()
+    plt.clf()
 
 
 def plot_theta_vs_n(filename):
     df = pd.read_csv(filename)
     x_label = df.columns[0]
     df = df[[x_label, 'N', 'Theta']]
-    print(df)
 
     df = df.drop(df[df['Solver'] == 'RBH'].index)
 
@@ -179,7 +266,7 @@ def plot_theta_vs_n(filename):
 
     plt.tight_layout()
     plt.savefig(os.path.splitext(filename)[0] + "_vs_theta.pdf")
-    plt.cla()
+    plt.clf()
 
 
 def plot_interaction_counts_vs_n(filename):
@@ -187,7 +274,6 @@ def plot_interaction_counts_vs_n(filename):
     x_label = df.columns[0]
     df['Total Interactions'] = df['Particle-Particle'] + df['Particle-Node'] + df['Node-Particle'] + df['Node-Node']
     df = df[[x_label, 'N', 'Total Interactions']]
-    print(df)
 
     df = df.drop(df[df['Solver'] == 'RBH'].index)
 
@@ -196,19 +282,18 @@ def plot_interaction_counts_vs_n(filename):
 
     plt.tight_layout()
     plt.savefig(os.path.splitext(filename)[0] + "_vs_interaction-count.pdf")
-    plt.cla()
+    plt.clf()
 
 
 def plot_interactions(filename):
     interaction_list = pd.read_csv(filename)
 
     node_node_interactions = interaction_list.loc[interaction_list['Type'] == "Node-Node"]
-    print(node_node_interactions)
 
     sns.scatterplot(data=node_node_interactions, x="Passive Size", y="Active Size")
     plt.tight_layout()
     plt.savefig(os.path.splitext(filename)[0] + "_ratios.pdf")
-    plt.cla()
+    plt.clf()
 
 
 def main():
@@ -228,16 +313,49 @@ def main():
     # plot_sweep_error('benchmarks/sweep-theta.csv')
     # plot_interactions('benchmarks/approximation-tracking.csv')
 
-    zoom_box = plot_field('benchmarks/sample-exact-1x1.csv')
-    plot_field('benchmarks/sample-exact-8x8.csv', zoom_box)
-
     # plot_interaction_counts("benchmarks/linear-bvh-descent-criterion.csv")
     # plot_times("benchmarks/linear-bvh-descent-criterion.csv")
 
+    print("Generating keys for field plots")
+    field_key()
+    field_error_key()
+
+    print("Plotting exact fields")
+    zoom_box = plot_field('benchmarks/sample-exact-zoom.csv')
+    plot_field('benchmarks/sample-exact.csv', zoom_box)
+
+    print("Plotting multipole moment approximations")
+    plot_field('benchmarks/sample-monopole-moment.csv')
+    plot_field_error('benchmarks/sample-monopole-moment-error.csv')
+    plot_field_error('benchmarks/sample-monopole-moment-extended-error.csv')
+    plot_field('benchmarks/sample-net-quadrupole-moment.csv')
+    plot_field('benchmarks/sample-quadrupole-moment.csv')
+    plot_field_error('benchmarks/sample-quadrupole-moment-error.csv')
+    plot_field('benchmarks/sample-octupole-moment.csv')
+    plot_field_error('benchmarks/sample-octupole-moment-error.csv')
+    plot_field('benchmarks/sample-hexadecupole-moment.csv')
+    plot_field_error('benchmarks/sample-hexadecupole-moment-error.csv')
+
+    print("Plotting multipole field approximations")
+    plot_field('benchmarks/sample-vector-field.csv')
+    plot_field_error('benchmarks/sample-vector-field-error.csv')
+    plot_field('benchmarks/sample-net-quadrupole-field.csv')
+    plot_field('benchmarks/sample-quadrupole-field.csv')
+    plot_field_error('benchmarks/sample-quadrupole-field-error.csv')
+    plot_field_error('benchmarks/sample-quadrupole-field-zoom-error.csv')
+    plot_field('benchmarks/sample-octupole-field.csv')
+    plot_field_error('benchmarks/sample-octupole-field-error.csv')
+    plot_field('benchmarks/sample-hexadecupole-field.csv')
+    plot_field_error('benchmarks/sample-hexadecupole-field-error.csv')
+    plot_field('benchmarks/sample-triacontadyupole-field.csv')
+    plot_field_error('benchmarks/sample-triacontadyupole-field-error.csv')
+
+    print("Plotting solver benchmarks")
     plot_times_vs_n("benchmarks/all-solvers-random-data.csv")
     plot_theta_vs_n("benchmarks/all-solvers-random-data.csv")
     plot_interaction_counts_vs_n("benchmarks/all-solvers-random-data.csv")
 
+    print("Plotting tree benchmarks")
     plot_times_vs_n("benchmarks/tree-construction-agora-data.csv")
     plot_times_vs_n("benchmarks/tree-construction-random-data.csv")
 
